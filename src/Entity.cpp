26190454278087e1dc230f8055b5f3b4df02bb16
Jonathan Moallem <jdmoallem@gmail.com>
2020-11-30 22:21:56 +1100

    Added entity base class to project

---
+++ src/Entity.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Jonathan Moallem on 30/11/20.
+//
+
+#include "Entity.h"
+#include <iostream>
---
+++ src/Entity.h
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan Moallem on 30/11/20.
+//
+
+#ifndef A_DARK_DISCOMFORT_ENTITY_H
+#define A_DARK_DISCOMFORT_ENTITY_H
+
+
+class Entity
+{
+public:
+    virtual void onUpdate() = 0;
+    virtual void onDraw() = 0;
+};
+
+
+#endif //A_DARK_DISCOMFORT_ENTITY_H
