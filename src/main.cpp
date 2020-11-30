4b3fbce41ddf3b916d4641f74887dcea554c8081
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-01 00:42:09 +1100

    Moved main render and update steps into functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,53 @@
 #include "Entity.h"
 #include "Cube.h"
 
-int main() {
-    
+// Globals
+std::vector<Entity*> entities;
+raylib::Camera3D camera;
+
+void beginPlay()
+{
+    // Instantiate world objects
+    camera = raylib::Camera3D(
+            raylib::Vector3(0.f, 10.f, 10.f),
+            raylib::Vector3::Zero(),
+            raylib::Vector3(0.f, 1.f, 0.f),
+            45.f,
+            CAMERA_PERSPECTIVE
+    );
+    entities.push_back(new Cube(raylib::Vector3::Zero()));
+}
+
+void updateWorld()
+{
+    // Update entities
+    for (auto& entity : entities)
+    {
+        entity->onUpdate();
+    }
+    camera.Update();
+}
+
+void renderWorld()
+{
+    // Draw entities
+    for (auto& entity : entities)
+    {
+        entity->onDraw();
+    }
+
+    DrawGrid(10, 1.0f);
+}
+
+void renderUI()
+{
+    // Draw UI
+    DrawText("Welcome to the 3rd dimension", 10.f, 40.f, 20.f, DARKGRAY);
+    DrawFPS(10.f, 10.f);
+}
+
+int main()
+{
     // Initialization
     int screenWidth = 800;
     int screenHeight = 450;
@@ -12,49 +57,23 @@ int main() {
     raylib::Color bg = RAYWHITE;
     raylib::Window w = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
 
-    raylib::Camera3D camera = raylib::Camera3D(
-            raylib::Vector3(0.f, 10.f, 10.f),
-            raylib::Vector3::Zero(),
-            raylib::Vector3(0.f, 1.f, 0.f),
-            45.f,
-            CAMERA_PERSPECTIVE
-    );
-
     w.SetTargetFPS(60);
 
-    std::vector<Entity*> entities;
-    entities.push_back(new Cube(raylib::Vector3::Zero()));
+    beginPlay();
 
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
-
-        for (auto& entity : entities)
-        {
-            entity->onUpdate();
-        }
-
-        camera.Update();
+        updateWorld();
 
         // Draw
         w.BeginDrawing();
-            bg.ClearBackground();
-
-            camera.BeginMode3D();
-
-                for (auto& entity : entities)
-                {
-                    entity->onDraw();
-                }
-
-                DrawGrid(10, 1.0f);
-
-            camera.EndMode3D();
-
-            DrawText("Welcome to the 3rd dimension", 10.f, 40.f, 20.f, DARKGRAY);
-            DrawFPS(10.f, 10.f);
-
+        bg.ClearBackground();
+        camera.BeginMode3D();
+        renderWorld();
+        camera.EndMode3D();
+        renderUI();
         w.EndDrawing();
     }
 
