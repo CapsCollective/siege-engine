b5ef9ca2dee00916ebf1d4e41f8283954ae8a14e
Jonathan Moallem <jdmoallem@gmail.com>
2020-11-30 22:20:18 +1100

    Migrated main file to C++ conventions

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,39 @@ int main() {
     int screenWidth = 800;
     int screenHeight = 450;
 
-    raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    
-    SetTargetFPS(60);
+    raylib::Color bg = RAYWHITE;
+    raylib::Window w = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
 
+    raylib::Camera3D camera = raylib::Camera3D(
+            raylib::Vector3(0.f, 10.f, 10.f),
+            raylib::Vector3::Zero(),
+            raylib::Vector3(0.f, 1.f, 0.f),
+            45.f,
+            CAMERA_PERSPECTIVE
+    );
+
+    w.SetTargetFPS(60);
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
 
-        // TODO: Update your variables here
+
+        camera.Update();
 
         // Draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
-        EndDrawing();
+        w.BeginDrawing();
+            bg.ClearBackground();
+
+            camera.BeginMode3D();
+                DrawGrid(10, 1.0f);
+
+            camera.EndMode3D();
+
+            DrawText("Welcome to the 3rd dimension", 10.f, 40.f, 20.f, DARKGRAY);
+            DrawFPS(10.f, 10.f);
+
+        w.EndDrawing();
     }
 
     return 0;
