b5767720fa4a088f2309fade637312f74a6e7c49
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-01 14:04:21 +1100

    Un-refactored, cleaned up, and commented main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
 #include <raylib-cpp.hpp>
 #include <vector>
-#include "Entity.h"
-#include "Cube.h"
+#include "entities/Entity.h"
+#include "entities/Cube.h"
 
-// Globals
-std::vector<Entity*> entities;
-raylib::Camera3D camera;
-
-void beginPlay()
+int main()
 {
+    // Initialise window
+    int screenWidth = 800;
+    int screenHeight = 450;
+    raylib::Color bg = RAYWHITE;
+    raylib::Window w = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+    w.SetTargetFPS(60);
+
+    // Define entity container
+    std::vector<Entity*> entities;
+    raylib::Camera3D camera;
+
     // Instantiate world objects
+    entities.push_back(new Cube(raylib::Vector3::Zero()));
     camera = raylib::Camera3D(
             raylib::Vector3(0.f, 10.f, 10.f),
             raylib::Vector3::Zero(),
@@ -17,63 +25,35 @@ void beginPlay()
             45.f,
             CAMERA_PERSPECTIVE
     );
-    entities.push_back(new Cube(raylib::Vector3::Zero()));
-}
 
-void updateWorld()
-{
-    // Update entities
-    for (auto& entity : entities)
+    // Run main game loop until close button or ESC key
+    while (!w.ShouldClose())
     {
-        entity->onUpdate();
-    }
-    camera.Update();
-}
+        // Update entities
+        for (auto& entity : entities)
+        {
+            entity->onUpdate();
+        }
+        camera.Update();
 
-void renderWorld()
-{
-    // Draw entities
-    for (auto& entity : entities)
-    {
-        entity->onDraw();
-    }
-
-    DrawGrid(10, 1.0f);
-}
-
-void renderUI()
-{
-    // Draw UI
-    DrawText("Welcome to the 3rd dimension", 10.f, 40.f, 20.f, DARKGRAY);
-    DrawFPS(10.f, 10.f);
-}
-
-int main()
-{
-    // Initialization
-    int screenWidth = 800;
-    int screenHeight = 450;
-
-    raylib::Color bg = RAYWHITE;
-    raylib::Window w = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-
-    w.SetTargetFPS(60);
-
-    beginPlay();
-
-    // Main game loop
-    while (!w.ShouldClose()) // Detect window close button or ESC key
-    {
-        // Update
-        updateWorld();
-
-        // Draw
+        // Begin drawing to screen
         w.BeginDrawing();
         bg.ClearBackground();
         camera.BeginMode3D();
-        renderWorld();
+
+        // Draw entities
+        for (auto& entity : entities)
+        {
+            entity->onDraw();
+        }
+        DrawGrid(10, 1.0f);
+
         camera.EndMode3D();
-        renderUI();
+
+        // Draw UI
+        DrawText("Move with the arrow keys", 10.f, 40.f, 20.f, DARKGRAY);
+        DrawFPS(10.f, 10.f);
+
         w.EndDrawing();
     }
 
