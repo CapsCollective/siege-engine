d7b195c563ed648b67234abe9b83e279e7397091
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-08 23:12:42 +1100

    Added player controller class

---
+++ src/entities/PlayerController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan Moallem on 9/12/20.
+//
+
+#include "PlayerController.h"
---
+++ src/entities/PlayerController.h
@@ -0,0 +1,15 @@
+//
+// Created by Jonathan Moallem on 9/12/20.
+//
+
+#ifndef A_DARK_DISCOMFORT_PLAYERCONTROLLER_H
+#define A_DARK_DISCOMFORT_PLAYERCONTROLLER_H
+
+
+class PlayerController
+{
+
+};
+
+
+#endif //A_DARK_DISCOMFORT_PLAYERCONTROLLER_H
