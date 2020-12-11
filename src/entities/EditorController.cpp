0b4a5d06c3b83b8759aca5108ced24330ef38d8b
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-11 16:47:45 +1100

    Implemented editor controller

--- src/entities/EditorController.cpp
+++ src/entities/EditorController.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "EditorController.h"
+
+void EditorController::onUpdate()
+{
+    if (!camera) { return; }
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        Ray ray = camera->GetMouseRay(GetMousePosition());
+
+        selectedEntity = nullptr;
+        for (auto& entity : *entities)
+        {
+            if (CheckCollisionRayBox(ray, entity->getBoundingBox()))
+            {
+                selectedEntity = entity;
+            }
+        }
+    }
+}
+
+void EditorController::onDraw()
+{
+    if (!selectedEntity) { return; }
+
+    DrawLine3D(selectedEntity->getPosition(), selectedEntity->getPosition() + raylib::Vector3(3.f, 0.f, 0.f), RED);
+    DrawLine3D(selectedEntity->getPosition(), selectedEntity->getPosition() + raylib::Vector3(0.f, 3.f, 0.f), GREEN);
+    DrawLine3D(selectedEntity->getPosition(), selectedEntity->getPosition() + raylib::Vector3(0.f, 0.f, 3.f), BLUE);
+}
+
+void EditorController::onUIDraw()
+{
+    if (!selectedEntity) { return; }
+
+    DrawText(FormatText("Selected Entity Position: <%f, %f, %f>",
+                        selectedEntity->getPosition().x,
+                        selectedEntity->getPosition().y,
+                        selectedEntity->getPosition().z),
+             10.f, 70.f, 20.f, PINK);
+}
