db2cb0b6a0f80c2b5645c5c8421cd1e8f07ce559
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-11 16:47:45 +1100

    Implemented editor controller

--- src/entities/EditorController.h
+++ src/entities/EditorController.h
@@ -5,12 +5,42 @@
 #ifndef A_DARK_DISCOMFORT_EDITORCONTROLLER_H
 #define A_DARK_DISCOMFORT_EDITORCONTROLLER_H
 
+#include <Camera3D.hpp>
+#include <vector>
+#include "Entity.h"
 
+class EditorController : public Entity
+{
+public:
 
-class EditorController {
+    // Constructors
 
+    explicit EditorController(raylib::Camera3D* camera, std::vector<Entity*>* entities) :
+    selectedEntity(nullptr),
+    entities(entities),
+    camera(camera)
+    {};
+
+protected:
+
+    // Protected overrides
+
+    void onUpdate() override;
+
+    void onDraw() override;
+
+    void onUIDraw() override;
+
+private:
+
+    // Private fields
+
+    raylib::Camera3D* camera;
+
+    std::vector<Entity*>* entities;
+
+    Entity* selectedEntity;
 };
-
 
 
 #endif //A_DARK_DISCOMFORT_EDITORCONTROLLER_H
