9e5a429a598bf4b3adb75edad7bebe0491ee2d3b
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-11 16:47:57 +1100

    Updated main to use editor controller

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <Window.hpp>
 #include <Color.hpp>
 #include <Camera3D.hpp>
-#include "entities/Entity.h"
 #include "entities/Player.h"
 #include "entities/Geometry.h"
+#include "entities/EditorController.h"
 
 int main()
 {
@@ -13,19 +13,14 @@ int main()
     int screenWidth = 800;
     int screenHeight = 450;
     raylib::Color bg = RAYWHITE;
-    raylib::Window w = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    w.SetTargetFPS(60);
+    raylib::Window window = raylib::Window(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
+    window.SetTargetFPS(60);
 
     // Define entity container
     std::vector<Entity*> entities;
-    raylib::Camera3D camera;
 
-    // Instantiate world objects
-    entities.push_back(new Player());
-    entities.push_back(new Geometry(
-            raylib::Vector3::Zero(),
-            raylib::Vector3(5.f, 0.1f, 5.f))
-    );
+    // Create camera
+    raylib::Camera3D camera;
     camera = raylib::Camera3D(
             raylib::Vector3(0.f, 10.f, 10.f),
             raylib::Vector3::Zero(),
@@ -34,8 +29,16 @@ int main()
             CAMERA_PERSPECTIVE
     );
 
+    // Instantiate world objects
+    entities.push_back(new Player());
+    entities.push_back(new Geometry(
+            raylib::Vector3::Zero(),
+            raylib::Vector3(5.f, 0.1f, 5.f))
+    );
+    entities.push_back(new EditorController(&camera, &entities));
+
     // Run main game loop until close button or ESC key
-    while (!w.ShouldClose())
+    while (!window.ShouldClose())
     {
         // Update entities
         for (auto& entity : entities)
@@ -45,7 +48,7 @@ int main()
         camera.Update();
 
         // Begin drawing to screen
-        w.BeginDrawing();
+        window.BeginDrawing();
         bg.ClearBackground();
         camera.BeginMode3D();
 
@@ -54,15 +57,22 @@ int main()
         {
             entity->onDraw();
         }
+
         DrawGrid(10, 1.0f);
 
         camera.EndMode3D();
 
-        // Draw UI
-        DrawText("Move with the arrow keys", 10.f, 40.f, 20.f, DARKGRAY);
+        // Begin drawing UI to screen
         DrawFPS(10.f, 10.f);
+        DrawText("Move with the arrow keys", 10.f, 40.f, 20.f, DARKGRAY);
 
-        w.EndDrawing();
+        // UI Draw entities
+        for (auto& entity : entities)
+        {
+            entity->onUIDraw();
+        }
+
+        window.EndDrawing();
     }
 
     return 0;
