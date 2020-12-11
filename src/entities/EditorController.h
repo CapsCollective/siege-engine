5e00ddb015b3a48a1586b622f0bbe4b6fa666150
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-11 16:46:29 +1100

    Added editor controller

---
+++ src/entities/EditorController.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Moallem on 11/12/20.
+//
+
+#ifndef A_DARK_DISCOMFORT_EDITORCONTROLLER_H
+#define A_DARK_DISCOMFORT_EDITORCONTROLLER_H
+
+
+
+class EditorController {
+
+};
+
+
+
+#endif //A_DARK_DISCOMFORT_EDITORCONTROLLER_H
