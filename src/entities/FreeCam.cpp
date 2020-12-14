ae38fffb2534dec0a2e4bd3022f09ff535c385bc
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-14 16:08:18 +1100

    Fixed up freecam rotations

--- src/entities/FreeCam.cpp
+++ src/entities/FreeCam.cpp
@@ -8,6 +8,11 @@ void FreeCam::OnUpdate()
 {
     if (!camera) return;
 
+    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
+    {
+        // TODO some camera look code
+    }
+
     // Get movement as vector
     raylib::Vector3 move = {
             -(float)IsKeyDown(KEY_A) + (float)IsKeyDown(KEY_D),
@@ -15,12 +20,10 @@ void FreeCam::OnUpdate()
             -(float)IsKeyDown(KEY_W) + (float)IsKeyDown(KEY_S),
     };
 
-    // Update the camera object in raylib
-    camera->Update();
+    // Calculate the rotation
+    raylib::Vector3 rotation = raylib::Vector3(0.f, -1.f, -1.f);
 
-    // Set the new position of the camera
-    raylib::Vector3 initialPosition = camera->GetPosition();
-    camera->SetPosition(initialPosition + move.Normalize() * speed * GetFrameTime());
-//    initialPosition = camera->GetPosition();
-//    camera->SetTarget(raylib::Vector3(0.f, 0.f, 0.f));
+    // Set the new position and look rotation of the camera
+    camera->SetPosition(raylib::Vector3(camera->GetPosition()) + move.Normalize() * speed * GetFrameTime());
+    camera->SetTarget(raylib::Vector3(camera->GetPosition()) + rotation);
 }
