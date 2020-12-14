9573c369e4ad638b7e876060bb9aaaad5f6ea090
Jonathan Moallem <jdmoallem@gmail.com>
2020-12-14 15:05:40 +1100

    Added freecam entity class

--- src/entities/FreeCam.h
+++ src/entities/FreeCam.h
@@ -5,12 +5,36 @@
 #ifndef A_DARK_DISCOMFORT_FREECAM_H
 #define A_DARK_DISCOMFORT_FREECAM_H
 
+#include <Camera3D.hpp>
+#include "Entity.h"
 
+class FreeCam : public Entity
+{
+public:
 
-class FreeCam {
+    // Constructors
 
+    explicit FreeCam(raylib::Camera3D* camera) :
+    speed(10.0f),
+    camera(camera)
+    {
+        camera->SetMode(CAMERA_CUSTOM);
+    };
+
+protected:
+
+    // Protected overrides
+
+    void OnUpdate() override;
+
+private:
+
+    // Private fields
+
+    float speed;
+
+    raylib::Camera3D* camera;
 };
-
 
 
 #endif //A_DARK_DISCOMFORT_FREECAM_H
