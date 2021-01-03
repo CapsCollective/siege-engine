d86e3283dc6542e77028d8c463881bc719caf965
Aryeh <aryehzinn@hotmail.com>
2021-01-03 21:24:30 +1100

    Finished documentation on the resource manager

--- src/resources/ResourceManager.h
+++ src/resources/ResourceManager.h
@@ -11,7 +11,6 @@
 // A macro which defines what a resource map actually looks like
 #define RESOURCE_MAP std::map<std::string, std::variant<Model, Texture2D>>
 
-// TODO comment API functions
 class ResourceManager
 {
 public:
@@ -50,28 +49,54 @@ public:
         resources.insert({path, resource});
     };
 
+    /**
+     * Returns a reference to a resource.
+     * @tparam T - the type of resource which needs to be returned.
+     * @param path - the path of the resource in the file system.
+     * @return - returns a reference to the designated resource.
+     */
     template<typename T>
     static T& Get(const std::string& path)
     {
+        // Unwraps the variant in the given path
         return std::get<T>(resources.at(path));
     };
 
+    /**
+     * Returns a pointer to a resource
+     * @tparam T - the type of resource which needs to be returned.
+     * @param path - the path of the resource in the file system.
+     * @return - returns a pointer to the designated resource.
+     */
     template<typename T>
     static T* GetRef(const std::string& path)
     {
+        // Upwraps the variant in the given path and returns a pointer.
         return &std::get<T>(resources[path]);
     };
 
+    /**
+     * Frees all resources in the resource map.
+     */
     static void FreeAllResources();
 
+    /**
+     * Adds all resources to a veector which is cleared at the end of the frame.
+     */
     static void ClearResources();
 
 private:
 
     // Private fields
 
+    /**
+     * A static map for all our resources.
+     */
     static RESOURCE_MAP resources;
 
+    /**
+     * A vector of all resources which we want to free at the end of the frame.
+     */
     static std::vector<std::variant<Model, Texture2D>*> freedResources;
 };
 
