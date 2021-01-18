8d461d114ff60cdaad314b735d82435b5355d57e
Jonathan Moallem <jdmoallem@gmail.com>
2021-01-18 13:48:32 +1100

    Created basic widget system structure

---
+++ src/systems/widget/Widget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan Moallem on 18/1/21.
+//
+
+#include "Widget.h"
---
+++ src/systems/widget/WidgetStorage.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Moallem on 18/1/21.
+//
+
+#ifndef A_DARK_DISCOMFORT_WIDGETSTORAGE_H
+#define A_DARK_DISCOMFORT_WIDGETSTORAGE_H
+
+
+
+class WidgetStorage {
+
+};
+
+
+
+#endif //A_DARK_DISCOMFORT_WIDGETSTORAGE_H
