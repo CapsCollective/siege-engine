fecfeb262e54b12013acfbca1101e61eca66bab5
Jonathan Moallem <jdmoallem@gmail.com>
2021-01-18 13:55:51 +1100

    Fleshed out widget and widget storage classes

--- src/systems/widget/Widget.h
+++ src/systems/widget/Widget.h
@@ -1,16 +1,54 @@
-//
-// Created by Jonathan Moallem on 18/1/21.
-//
-
 #ifndef A_DARK_DISCOMFORT_WIDGET_H
 #define A_DARK_DISCOMFORT_WIDGET_H
 
+class Widget
+{
+public:
 
+    /**
+     * A virtual update method to be overridden for widget
+     * drawing instructions
+     */
+    virtual void OnDraw() {};
 
-class Widget {
+    /**
+     * Queues the widget to be freed from storage safely at
+     * the end of a frame
+     */
+    void QueueFree();
 
+    /**
+     * Unsafely removes the widget from storage immediately
+     * @warning This method is very unsafe and QueueFree()
+     *          should be used wherever possible instead
+     */
+    void Free();
+
+protected:
+
+    // Constructors
+
+    /**
+     * Zero-param constructor for Widget, initialises
+     * z-index to zero
+     */
+    Widget() :
+    Widget(0)
+    {};
+
+    /**
+     * Delegate constructor for Widget
+     * @param zIndex
+     */
+    explicit Widget(int zIndex) :
+    zIndex(zIndex)
+    {};
+
+    /**
+     * The widget z-index for render order
+     */
+    int zIndex;
 };
-
 
 
 #endif //A_DARK_DISCOMFORT_WIDGET_H
--- src/systems/widget/WidgetStorage.h
+++ src/systems/widget/WidgetStorage.h
@@ -1,16 +1,26 @@
-//
-// Created by Jonathan Moallem on 18/1/21.
-//
-
 #ifndef A_DARK_DISCOMFORT_WIDGETSTORAGE_H
 #define A_DARK_DISCOMFORT_WIDGETSTORAGE_H
 
+#include <vector>
 
+class WidgetStorage
+{
+public:
 
-class WidgetStorage {
+    // TODO implement widget storage
+    static void Register(class Widget* widget);
 
+    static void Remove(Widget* widget);
+
+    static const std::vector<Widget*>& GetWidgets()
+    {
+        return widgets;
+    }
+
+private:
+
+    static std::vector<Widget*> widgets;
 };
-
 
 
 #endif //A_DARK_DISCOMFORT_WIDGETSTORAGE_H
