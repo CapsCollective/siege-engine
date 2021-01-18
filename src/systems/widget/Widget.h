912a54be0503c89ba12fdc53f64a94b8473d8829
Jonathan Moallem <jdmoallem@gmail.com>
2021-01-18 13:48:32 +1100

    Created basic widget system structure

---
+++ src/systems/widget/Widget.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Moallem on 18/1/21.
+//
+
+#ifndef A_DARK_DISCOMFORT_WIDGET_H
+#define A_DARK_DISCOMFORT_WIDGET_H
+
+
+
+class Widget {
+
+};
+
+
+
+#endif //A_DARK_DISCOMFORT_WIDGET_H
