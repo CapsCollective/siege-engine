193aa7fb9a6c0ddc32511fbf6598a5ffb4a28f0e
Jonathan Moallem <jdmoallem@gmail.com>
2021-01-29 14:01:16 +1100

    Added basic classes for collision system

---
+++ src/systems/collision/CollisionRegister.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan Moallem on 29/1/21.
+//
+
+#include "CollisionRegister.h"
---
+++ src/systems/collision/CollisionRegister.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Moallem on 29/1/21.
+//
+
+#ifndef A_DARK_DISCOMFORT_COLLISIONREGISTER_H
+#define A_DARK_DISCOMFORT_COLLISIONREGISTER_H
+
+
+
+class CollisionRegister {
+
+};
+
+
+
+#endif //A_DARK_DISCOMFORT_COLLISIONREGISTER_H
