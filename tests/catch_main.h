eae3a9a567633b42aec7627d318eb01785d41bf8
Jonathan Moallem <jdmoallem@icloud.com>
2021-06-20 19:13:55 +1000

    Added entity system test files

---
+++ tests/catch_main.h
@@ -0,0 +1,7 @@
+#ifndef A_DARK_DISCOMFORT_CATCH_MAIN_H
+#define A_DARK_DISCOMFORT_CATCH_MAIN_H
+
+// Tell Catch to provide a main() - only do this in one cpp file!
+#define CATCH_CONFIG_MAIN
+
+#endif //A_DARK_DISCOMFORT_CATCH_MAIN_H
