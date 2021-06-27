6d1c38038e21dbd5cbe768f4050afdfb32122e03
Jonathan Moallem <jdmoallem@icloud.com>
2021-06-27 17:24:08 +1000

    Added log levels and colours to logging system

--- src/utils/Logging.h
+++ src/utils/Logging.h
@@ -1,14 +1,52 @@
 #ifndef A_DARK_DISCOMFORT_LOGGING_H
 #define A_DARK_DISCOMFORT_LOGGING_H
 
+#include "Macros.h"
 #include <iostream>
 
-// filename, line
-// log level display
-#define LINE_LOC __FILE__ ":" TOSTRING(__LINE__)
-#define _LOG(log_level, message, ...) std::printf(log_level " at [" LINE_LOC "] " message, ##__VA_ARGS__)
-#define LOG_ERROR(message, ...) _LOG("ERROR", message, __VA_ARGS__)
-#define LOG_WARNING(message, ...) _LOG("WARNING", message, __VA_ARGS__)
-#define LOG_INFO(message, ...) _LOG("INFO", message, __VA_ARGS__)
+// Define terminal colour wrapping macros
+#define _CC_WRAP_TEXT_RED(text) "\x1B[31m" text "\033[0m"
+#define _CC_WRAP_TEXT_YELLOW(text) "\x1B[33m" text "\033[0m"
+#define _CC_WRAP_TEXT_GREY(text) "\x1B[37m" text "\033[0m"
+
+// Define logging helper macros
+#define _CC_LINE_LOC __FILE__ ":" TOSTRING(__LINE__)
+#define _CC_LOG(log_level, colour_macro, message, ...) \
+    std::printf(colour_macro(log_level " at [" _CC_LINE_LOC "] " message) "\n", ##__VA_ARGS__)
+
+// Determine log level
+#ifdef CC_LOG_LEVEL
+#if CC_LOG_LEVEL==0
+#define _CC_DEFINE_LOG_ERROR
+#elif CC_LOG_LEVEL==1
+#define _CC_DEFINE_LOG_ERROR
+#define _CC_DEFINE_LOG_WARNING
+#elif CC_LOG_LEVEL==2
+#define _CC_DEFINE_LOG_ERROR
+#define _CC_DEFINE_LOG_WARNING
+#define _CC_DEFINE_LOG_INFO
+#endif
+#endif
+
+// Define error logging macro
+#ifdef _CC_DEFINE_LOG_ERROR
+#define CC_LOG_ERROR(message, ...) _CC_LOG("ERROR", _CC_WRAP_TEXT_RED, message, ##__VA_ARGS__)
+#else
+#define CC_LOG_ERROR(...)
+#endif
+
+// Define warning logging macro
+#ifdef _CC_DEFINE_LOG_WARNING
+#define CC_LOG_WARNING(message, ...) _CC_LOG("WARNING", _CC_WRAP_TEXT_YELLOW, message, ##__VA_ARGS__)
+#else
+#define CC_LOG_WARNING(...)
+#endif
+
+// Define info logging macro
+#ifdef _CC_DEFINE_LOG_INFO
+#define CC_LOG_INFO(message, ...) _CC_LOG("INFO", _CC_WRAP_TEXT_GREY, message, ##__VA_ARGS__)
+#else
+#define CC_LOG_INFO(...)
+#endif
 
 #endif //A_DARK_DISCOMFORT_LOGGING_H
