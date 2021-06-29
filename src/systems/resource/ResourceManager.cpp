ee55a2ed3d77f545241afb6a01e3b8a672a924c2
Aryeh <aryehzinn@hotmail.com>
2021-06-29 23:05:25 +1000

    Removed comments from ResourceManager

--- src/systems/resource/ResourceManager.cpp
+++ src/systems/resource/ResourceManager.cpp
@@ -7,37 +7,30 @@ ResourceList ResourceManager::freedResources;
 
 void ResourceManager::ClearResources()
 {
-    // Get every resource in the map
     for (auto& resource : resources)
     {
-        // Push the resource to the freeResources vector for unloading at the end of the frame
         freedResources.push_back(&resource.second);
     }
 }
 
 void ResourceManager::FreeResources()
 {
-    // If we have resources to free
     if (freedResources.empty()) return;
 
     // Iterate over the vector backwards to avoid memory issues
     for (auto i = freedResources.rbegin(); i != freedResources.rend(); ++i)
     {
-        // Check whether the resource in question is a Model
+        // Check what type the variant holds - requires two levels
+        // of de - referencing (once for the iterator once for the vector element)
         if (std::holds_alternative<Model>(**i))
         {
-            // Unload the resource as a model
             UnloadModel(std::get<Model>(**i));
         }
-        // Check whether the resource in question is a Texture2D
         else if (std::holds_alternative<Texture2D>(**i))
         {
-            // Unload the resource as a texture
             UnloadTexture(std::get<Texture2D>(**i));
         }
     }
-    // Empty out the freed resources vector
     freedResources.clear();
-    // Clear out the resources in the resources map
     resources.clear();
 }
