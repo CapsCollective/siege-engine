8b1a38f9ff53a33b8ee8d8de9d3f67e0b2b6f22b
Aryeh <aryehzinn@hotmail.com>
2021-07-01 22:21:20 +1000

    Added SceneManager unit tests, alongside test files and entities

---
+++ tests/data/TestEntity.cpp
@@ -0,0 +1,18 @@
+#include "TestEntity.h"
+#include "../src/systems/scene/SceneSerialiser.h"
+
+const std::string TestEntity::ENTITY_NAME("TestEntity");
+
+static std::string Serialise(Entity* entity)
+{
+    return "";
+}
+
+static Entity* Deserialise(const EntityData& data, const std::vector<std::string>& args)
+{
+    return new TestEntity(data.position, data.rotation);
+}
+
+REGISTER_SERIALISATION_INTERFACE(TestEntity::ENTITY_NAME, Serialise, Deserialise);
+
+
