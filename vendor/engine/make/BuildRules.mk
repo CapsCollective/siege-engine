5608c5c774f74b23324d81713b0a8e32a1a6c118
Jonathan Moallem <jdmoallem@icloud.com>
2021-07-18 14:42:59 +1000

    Fixed up Makefile target override errors

--- vendor/engine/make/BuildRules.mk
+++ vendor/engine/make/BuildRules.mk
@@ -1,11 +1,19 @@
 # Link the object files and create an executable
-$(target): $(objects)
+$(target)-default: $(objects)
 	$(CXX) $(objects) -o $(target) $(linkFlags)
 
 # Add all rules from dependency files
 -include $(depends)
 
 # Compile object files to the build directory
-$(buildDir)/%.o: $(srcDir)/%.cpp
+$(buildDir)/%.o-default: $(srcDir)/%.cpp
 	$(MKDIR) $(call platformpth, $(@D))
-	$(CXX) -MMD -MP -c $(compileFlags) $< -o $@ $(CXXFLAGS)
+	$(CXX) -MMD -MP -c $(compileFlags) $< -o $(patsubst %-default, %, $@) $(CXXFLAGS)
+
+# Redirect unfound object targets to defaults
+%.o: %.o-default
+	@ true
+
+# Redirect unfound misc targets to defaults
+%: %-default
+	@ true
