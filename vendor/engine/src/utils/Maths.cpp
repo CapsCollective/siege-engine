c95cee449eb40b743755816a80b705015d0e6ff2
Jonathan Moallem <jdmoallem@icloud.com>
2021-07-21 01:26:59 +1000

    Added Vec3 and BoundedBox to utils

---
+++ vendor/engine/src/utils/Maths.cpp
@@ -0,0 +1,32 @@
+#include "Maths.h"
+#include <sstream>
+#include <iomanip>
+
+// Define static members
+const Vec3 Vec3::Zero = {0.f, 0.f, 0.f};
+const Vec3 Vec3::One = {1.f, 1.f, 1.f};
+
+Vec3 Vec3::Normalise() const
+{
+    float length = Length();
+    if (length == 0.f) length = 1.f;
+    return *this * 1.f/length;
+}
+
+std::string Vec3::ToString() const
+{
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << x  << "," << y << "," << z;
+    return ss.str();
+}
+
+bool BoundedBox::Intersects(const BoundedBox &other) const
+{
+    if ((max.x >= other.min.x) && (min.x <= other.max.x))
+    {
+        if ((max.y < other.min.y) || (min.y > other.max.y)) return false;
+        if ((max.z < other.min.z) || (min.z > other.max.z)) return false;
+    }
+    else return false;
+    return true;
+}
---
+++ vendor/engine/src/utils/Maths.h
@@ -0,0 +1,151 @@
+#ifndef A_DARK_DISCOMFORT_MATHS_H
+#define A_DARK_DISCOMFORT_MATHS_H
+
+#include "Macros.h"
+#include <string>
+#include <raylib/Vector3.hpp>
+
+// Define macros
+
+#define DEFINE_OPERATOR(op, lhsType, rhsType, body) \
+    lhsType& operator op(const rhsType& rhs) \
+    { \
+        body(op, SEMICOLON) \
+        return *this; \
+    }
+
+#define DEFINE_CONST_OPERATOR(op, lhsType, rhsType, body) \
+    lhsType operator op(const rhsType& rhs) const \
+    { \
+        return { \
+            body(op, COMMA) \
+        }; \
+    }
+
+#define VEC_OPERATOR_BODY_VEC(op, sep) \
+    x op rhs.x sep \
+    y op rhs.y sep \
+    z op rhs.z sep
+
+#define VEC_OPERATOR_BODY_FLOAT(op, sep) \
+    x op rhs sep \
+    y op rhs sep \
+    z op rhs sep
+
+struct Vec3
+{
+    // Public constants
+
+    static const Vec3 Zero;
+
+    static const Vec3 One;
+
+    // 'Structors
+
+    Vec3() :
+        Vec3(0.f, 0.f, 0.f)
+    {}
+
+    Vec3(float x, float y, float z) :
+        x(x),
+        y(y),
+        z(z)
+    {}
+
+    // Raylib conversions
+
+    Vec3(raylib::Vector3 vector) :
+            Vec3(vector.x, vector.y, vector.z)
+    {}
+
+    operator raylib::Vector3() const
+    {
+        return {x, y, z};
+    }
+
+    // Operator overloads
+
+    DEFINE_OPERATOR(+=, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_OPERATOR(-=, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_OPERATOR(*=, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_OPERATOR(/=, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+
+    DEFINE_OPERATOR(+=, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_OPERATOR(-=, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_OPERATOR(*=, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_OPERATOR(/=, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+
+    DEFINE_CONST_OPERATOR(+, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_CONST_OPERATOR(-, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_CONST_OPERATOR(*, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+    DEFINE_CONST_OPERATOR(/, Vec3, Vec3, VEC_OPERATOR_BODY_VEC)
+
+    DEFINE_CONST_OPERATOR(+, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_CONST_OPERATOR(-, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_CONST_OPERATOR(*, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+    DEFINE_CONST_OPERATOR(/, Vec3, float, VEC_OPERATOR_BODY_FLOAT)
+
+    // Public methods
+
+    std::string ToString() const;
+
+    Vec3 Normalise() const;
+
+    float Length() const
+    {
+        return sqrtf(x*x + y*y + z*z);
+    }
+
+    Vec3 XComp() const
+    {
+        return {x,0.f,0.f};
+    }
+
+    Vec3 YComp() const
+    {
+        return {0.f, y,0.f};
+    }
+
+    Vec3 ZComp() const
+    {
+        return {0.f,0.f,z};
+    }
+
+    // Public members
+
+    float x;
+    float y;
+    float z;
+};
+
+struct BoundedBox
+{
+    // 'Structors
+
+    BoundedBox() :
+        BoundedBox(Vec3(), Vec3())
+    {}
+
+    BoundedBox(Vec3 min, Vec3 max) :
+        min(min),
+        max(max)
+    {}
+
+    // Raylib conversions
+
+    operator BoundingBox() const
+    {
+        return {min, max};
+    }
+
+    // Public methods
+
+    bool Intersects(const BoundedBox& other) const;
+
+    // Public members
+
+    Vec3 min;
+    Vec3 max;
+};
+
+#endif //A_DARK_DISCOMFORT_MATHS_H
