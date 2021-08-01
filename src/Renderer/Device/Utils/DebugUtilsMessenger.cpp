3c2ae2caba622bbc8a4b31667b91b0ff8be52cdc
Aryeh <aryehzinn@hotmail.com>
2021-08-02 09:01:07 +1000

    Commented DebugUtilsMessenger file

--- src/Renderer/Device/Utils/DebugUtilsMessenger.cpp
+++ src/Renderer/Device/Utils/DebugUtilsMessenger.cpp
@@ -2,54 +2,69 @@
 
 namespace SnekVk::DebugUtilsMessenger
 {
-          // local callback functions
-  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-      VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-      VkDebugUtilsMessageTypeFlagsEXT messageType,
-      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-      void *pUserData) {
-    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
 
-    return VK_FALSE;
-  }
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+        void *pUserData) 
+    {
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
 
-  VkResult CreateMessenger(
-      VkInstance instance,
-      const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
-      const VkAllocationCallbacks *pAllocator,
-      VkDebugUtilsMessengerEXT *pDebugMessenger) {
-    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
-        instance,
-        "vkCreateDebugUtilsMessengerEXT");
-    if (func != nullptr) {
-      return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    } else {
-      return VK_ERROR_EXTENSION_NOT_PRESENT;
+        return VK_FALSE;
     }
-  }
 
-  void DestroyMessenger(
-      VkInstance instance,
-      VkDebugUtilsMessengerEXT debugMessenger,
-      const VkAllocationCallbacks *pAllocator) {
-    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
-        instance,
-        "vkDestroyDebugUtilsMessengerEXT");
-    if (func != nullptr) {
-      func(instance, debugMessenger, pAllocator);
+    VkResult CreateMessenger(
+        VkInstance instance,
+        const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+        const VkAllocationCallbacks *pAllocator,
+        VkDebugUtilsMessengerEXT *pDebugMessenger) 
+    {
+        // Vulkan does not load validation layer-related function by default. 
+        // vkGetIntstanceProcAddr allows us to query for a relevant function pointer
+        // and use that. We get the pointer, cast it to the type of function we want,
+        // then use it in the 'func' variable. 
+
+        // In our case, we're looking 'vkCreateDebugUtilsMessengerEXT' so we can create
+        // our debug messenger.
+        auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+            instance,
+            "vkCreateDebugUtilsMessengerEXT");
+        if (func != nullptr) {
+            return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+        } else {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
     }
-  }
 
-  void PopulateCreateInfo(
-      VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
-    createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-                                VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-                            VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-                            VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-    createInfo.pfnUserCallback = debugCallback;
-    createInfo.pUserData = nullptr;  // Optional
-  }
+    void DestroyMessenger(
+        VkInstance instance,
+        VkDebugUtilsMessengerEXT debugMessenger,
+        const VkAllocationCallbacks *pAllocator) 
+    {
+        // Please see 'CreateMessenger' for an explanation for what this is doing. 
+
+        // In this case, we're looking for the 'vkDestroyDebugUtilsMessengerEXT' function 
+        // so that we can destroy the debug messenger.
+        auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+            instance,
+            "vkDestroyDebugUtilsMessengerEXT");
+        if (func != nullptr) {
+            func(instance, debugMessenger, pAllocator);
+        }
+    }
+
+    void PopulateCreateInfo(
+        VkDebugUtilsMessengerCreateInfoEXT &createInfo) 
+    {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                                    VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                                VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+        createInfo.pUserData = nullptr;  // Optional
+    }
 }
--- src/Renderer/Device/Utils/DebugUtilsMessenger.h
+++ src/Renderer/Device/Utils/DebugUtilsMessenger.h
@@ -5,23 +5,57 @@
 
 namespace SnekVk::DebugUtilsMessenger
 {
-      // local callback functions
-  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-      VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-      VkDebugUtilsMessageTypeFlagsEXT messageType,
-      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-      void *pUserData);
+  /**
+   * A function pointer that acts as a callback for validation layers. 
+   * This allows us to specify any behaviours or functions that should
+   * be triggered if a validation error or warning are generated.
+   * 
+   * In our case, all this callback does is log the error to stdErr.
+   *   
+   * @param messageSeverity a bitmask specifying therequired message severity
+   * @param messageType a bitmask specifying the message type required
+   * @param pCallbackData the data being returned by the validationLayer
+   * @param pUserData a void pointer containing any data inserted for usage. 
+   **/
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+        void *pUserData);
 
-  VkResult CreateMessenger(
-      VkInstance instance,
-      const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
-      const VkAllocationCallbacks *pAllocator,
-      VkDebugUtilsMessengerEXT *pDebugMessenger);
+    /**
+     * Creates a DebugUtilsMessenger and returns the result to the 
+     * 'pDebugMessenger' variable.
+     * 
+     * @param instance the Vulkan instance being used to create the messenger.
+     * @param pCreateInfo the DebugUtilsMessengerCreateInfoEXT struct used to configure the messenger.
+     * @param pAllocator the memory allocator used to allocate the messenger.
+     * @param pDebugMessenger the debug messenger struct to be created and returned. 
+     **/
+    VkResult CreateMessenger(
+        VkInstance instance,
+        const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+        const VkAllocationCallbacks *pAllocator,
+        VkDebugUtilsMessengerEXT *pDebugMessenger);
 
-  void DestroyMessenger(
-      VkInstance instance,
-      VkDebugUtilsMessengerEXT debugMessenger,
-      const VkAllocationCallbacks *pAllocator);
+    /**
+     * De-allocates the memory of a DebugUtilsMessengerEXT struct. 
+     * 
+     * @param instance the instance used to create the debug messenger.
+     * @param debugMessenger the debugMessenger to be de-allocated
+     * @param pAllocator the allocator used to create the messenger
+     **/
+    void DestroyMessenger(
+        VkInstance instance,
+        VkDebugUtilsMessengerEXT debugMessenger,
+        const VkAllocationCallbacks *pAllocator);
 
-  void PopulateCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+    /**
+     * Populates a DebugUtilsMessengerCreateInfoEXT struct with pre-configured values. 
+     * In our case, we only care about warning and error validation layers.  
+     * We're also interested in general errors, optimisation errors, and validation errors. 
+     * 
+     * @param createInfo an empty DebugUtilsMessengerCreateInfoEXT struct to be populated.
+     **/
+    void PopulateCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
 }
