6f243de81c211fc5b57825f54850c9335f498897
aryeh <aryehzinn@hotmail.com>
2021-08-01 15:09:27 +1000

    Moved all rendering operations to the renderer class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include "Window/Window.h"
-#include "Renderer/Device/VulkanDevice.h"
-#include "Renderer/Pipeline/Pipeline.h"
-#include "Renderer/Swapchain/Swapchain.h"
 #include "Renderer/Renderer.h"
 
 #if (defined(_WIN32) || defined(_WIN64)) && defined(DEBUG)
@@ -27,28 +24,6 @@ int main()
     SnekVk::Window window("Snek", WIDTH, HEIGHT);
 
     SnekVk::Renderer renderer(window);
-
-    // Graphics pipeline creation
-    // TODO: Edit this to allow pipelines to be configured and passed into renderer.
-    VkPipelineLayout pipelineLayout;
-
-    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    pipelineLayoutInfo.setLayoutCount = 0; 
-    pipelineLayoutInfo.pSetLayouts = nullptr;
-    pipelineLayoutInfo.pushConstantRangeCount = 0;
-    pipelineLayoutInfo.pPushConstantRanges = nullptr;
-
-    SNEK_ASSERT(vkCreatePipelineLayout(renderer.GetDevice().Device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) == VK_SUCCESS, 
-        "Failed to create pipeline layout!");
-
-    auto pipelineConfig = SnekVk::Pipeline::DefaultPipelineConfig(renderer.GetSwapChain().GetWidth(), renderer.GetSwapChain().GetHeight());
-    pipelineConfig.renderPass = renderer.GetSwapChain().GetRenderPass();
-    pipelineConfig.pipelineLayout = pipelineLayout;
-
-    SnekVk::Pipeline pipeline(renderer.GetDevice(), "bin/shaders/simpleShader.vert.spv", "bin/shaders/simpleShader.frag.spv", pipelineConfig);
-
-    renderer.CreateCommandBuffers(pipeline);
     
     // Main loop
     while(!window.WindowShouldClose()) {
@@ -56,9 +31,7 @@ int main()
         renderer.DrawFrame();
     }
 
-    vkDeviceWaitIdle(renderer.GetDevice().Device());
-
-    vkDestroyPipelineLayout(renderer.GetDevice().Device(), pipelineLayout, nullptr);
+    renderer.ClearDeviceQueue();
 
     return 0;
 }
