ae1d07b1b587febbe8b2a66dc27d9a6eaf8d6803
Aryeh <aryehzinn@hotmail.com>
2021-08-03 09:22:50 +1000

    Added Copy function

--- src/Renderer/Utils/Array.h
+++ src/Renderer/Utils/Array.h
@@ -70,105 +70,102 @@ namespace SnekVk::Utils
         }
 
         private:
-            PointerType pointer;
+        
+        PointerType pointer;
     };
 
     template<typename T>
     class Array 
     {
         public:
-            using ValueType = T;
-            using Iterator = ArrayIterator<Array>;
+        using ValueType = T;
+        using Iterator = ArrayIterator<Array>;
         public:
-            Array() : size{0} {};
 
-            Array(size_t size) : size{size} 
-            {
-                data = new T[size];
-            }
+        Array() : size{0} {};
 
-            Array(uint32_t size) : size{static_cast<size_t>(size)} 
-            {
-                data = new T[this->size];
-            }
+        Array(size_t size) : size{size} 
+        {
+            data = new T[size];
+        }
 
-            Array(std::initializer_list<T> values)
-            {
-                data = new T[values.size()];
-                
-                size_t index = 0;
-                for (auto val : values)
-                {
-                    data[index] = val;
-                    index++;
-                }
+        Array(uint32_t size) : size{static_cast<size_t>(size)} 
+        {
+            data = new T[this->size];
+        }
 
-                size = values.size();
-            }
-
-            Array(const Array<T>& other)
-            {
-                if (data) delete [] data;
-
-                size = other.Size();
-                data = new T[other.Size()];
-
-                size_t index = 0; 
-                for (auto value : other)
-                {
-                    data[index] = value;
-                    index++;
-                }
-            }
-
-            void operator=(const Array<T>& other)
-            {
-                if (data) delete [] data;
-
-                data = new T[other.Size()];
-                size = other.Size();
-
-                size_t index = 0; 
-                for (auto value : other)
-                {
-                    data[index] = value;
-                    index++;
-                }
-            }
-
-            void operator=(Array<T>&& other)
-            {
-                if (data) delete [] data;
-
-                data = std::move(other.data);
-                other.data = nullptr;
-
-                size = std::move(other.Size());
-            }
-
-            ~Array() { 
-                if (data) 
-                {
-                    delete [] data;
-                } 
-            }
-
-            const size_t Size() const { return size; }
-            void SetSize(size_t size) { this->size = size; }
-
-            T& operator[] (size_t index) { return data[index]; }
-            const T& operator[] (size_t index) const { return data[index]; }
-
-            T* Data() { return data; }
-
-            Iterator begin() { return Iterator(data); }
-            Iterator end() { return Iterator(data + size); }
+        Array(std::initializer_list<T> values)
+        {
+            data = new T[values.size()];
             
-            const Iterator begin() const { return Iterator(data); }
-            const Iterator end() const { return Iterator(data + size); }
+            size_t index = 0;
+            for (auto val : values)
+            {
+                data[index] = val;
+                index++;
+            }
+
+            size = values.size();
+        }
+
+        Array(const Array<T>& other)
+        {
+            if (data) delete [] data;
+
+            Copy(other);
+        }
+
+        void operator=(const Array<T>& other)
+        {
+            if (data) delete [] data;
+
+            Copy(other);
+        }
+
+        void operator=(Array<T>&& other)
+        {
+            if (data) delete [] data;
+
+            data = std::move(other.data);
+            other.data = nullptr;
+
+            size = std::move(other.Size());
+        }
+
+        ~Array() { 
+            if (data) delete [] data;
+        }
+
+        const size_t Size() const { return size; }
+        void SetSize(size_t size) { this->size = size; }
+
+        T& operator[] (size_t index) { return data[index]; }
+        const T& operator[] (size_t index) const { return data[index]; }
+
+        T* Data() { return data; }
+
+        Iterator begin() { return Iterator(data); }
+        Iterator end() { return Iterator(data + size); }
+        
+        const Iterator begin() const { return Iterator(data); }
+        const Iterator end() const { return Iterator(data + size); }
 
         private:
-            size_t size;
-            T* data = nullptr;
+
+        size_t size;
+        T* data = nullptr;
+
+        void Copy(const Array<T>& other) 
+        {
+            data = new T[other.Size()];
+            size = other.Size();
+
+            size_t index = 0; 
+            for (auto value : other)
+            {
+                data[index] = value;
+                index++;
+            }
+        }      
     };
 }
