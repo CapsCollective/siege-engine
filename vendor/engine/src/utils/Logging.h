8659b0e9618ce01a2800dfd0476eea3f191941ac
Jonathan Moallem <jdmoallem@icloud.com>
2021-08-03 18:49:22 +1000

    Added logging for entity lifecycle

--- vendor/engine/src/utils/Logging.h
+++ vendor/engine/src/utils/Logging.h
@@ -51,6 +51,7 @@ namespace Logging
         DEFINE_VARIANT_TYPE(const size_t& data, std::to_string(data));
         DEFINE_VARIANT_TYPE(bool data, data ? "true" : "false");
         DEFINE_VARIANT_TYPE(const Entity& data,data.GetName());
+        DEFINE_VARIANT_TYPE(const GenerationalIndex& data,data.ToString());
         DEFINE_VARIANT_TYPE(const Vec3& data,
                             "Vector3(" + data.ToString() + ")");
 
