c424943b3728f30cdac4983f5fe2e825faf93599
Aryeh <aryehzinn@hotmail.com>
2021-08-10 09:55:19 +1000

    Added perspective camera transformations

--- src/Renderer/Camera/Camera.h
+++ src/Renderer/Camera/Camera.h
@@ -17,10 +17,18 @@ namespace SnekVk
 
         void SetPerspectiveProjection(float fovy, float aspect, float near, float far);
 
+        void SetViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+
+        void SetViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{0.f, -1.f, 0.f});
+
+        void SetViewYXZ(glm::vec3 position, glm::vec3 rotation);
+
         const glm::mat4& GetProjection() { return projectionMatrix; }
+        const glm::mat4& GetView() { return viewMatrix; }
 
         private:
         
         glm::mat4 projectionMatrix{1.f};
+        glm::mat4 viewMatrix{1.f};
     };
 }
