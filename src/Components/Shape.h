1a531d98169c538c3163e95b2b40f519c6bb671f
Aryeh <aryehzinn@hotmail.com>
2021-08-09 13:36:10 +1000

    Added 3D support, along with 3D rotations with euler angles

--- src/Components/Shape.h
+++ src/Components/Shape.h
@@ -1,23 +1,48 @@
 #pragma once
 
 #include "../Renderer/Renderer.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Components 
 {
-    struct Transform2D
+    struct Transform
     {
-        glm::vec2 position {0.0f};
-        glm::vec2 scale {1.0f, 1.0f};
-        float rotation {0};
+        glm::vec3 position {0.0f};
+        glm::vec3 scale {1.f, 1.f, 1.f};
+        glm::vec3 rotation {};
 
-        glm::mat2 transform() 
+        // Matrix corrsponds to Translate * Ry * Rx * Rz * Scale
+        // Rotations correspond to Tait-bryan angles of Y(1), X(2), Z(3)
+        // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
+        glm::mat4 transform() 
         { 
-            const float s = glm::sin(rotation);
-            const float c = glm::cos(rotation);
-            glm::mat2 rotMatrix{{c, s}, {-s, c}};
+            const float c3 = glm::cos(rotation.z);
+            const float s3 = glm::sin(rotation.z);
+            const float c2 = glm::cos(rotation.x);
+            const float s2 = glm::sin(rotation.x);
+            const float c1 = glm::cos(rotation.y);
+            const float s1 = glm::sin(rotation.y);
 
-            glm::mat2 scaleMat = {{scale.x, 0.0f}, {0.0f, scale.y}};
-            return rotMatrix * scaleMat;
+            return glm::mat4{
+            {
+                scale.x * (c1 * c3 + s1 * s2 * s3),
+                scale.x * (c2 * s3),
+                scale.x * (c1 * s2 * s3 - c3 * s1),
+                0.0f,
+            },
+            {
+                scale.y * (c3 * s1 * s2 - c1 * s3),
+                scale.y * (c2 * c3),
+                scale.y * (c1 * c3 * s2 + s1 * s3),
+                0.0f,
+            },
+            {
+                scale.z * (c2 * s1),
+                scale.z * (-s2),
+                scale.z * (c1 * c2),
+                0.0f,
+            },
+            {position.x, position.y, position.z, 1.0f}};
         };
     };
 
@@ -30,22 +55,26 @@ namespace Components
 
         ~Shape();
 
-        Transform2D& GetTransform() { return transform; };
+        Transform& GetTransform() { return transform; };
         glm::vec3& GetColor() { return fillColor; }
         SnekVk::Model* GetModel() { return model; }
+        glm::vec3& GetRotation() { return transform.rotation; }
 
         const SnekVk::Model::PushConstantData& GetPushConstantData() const { return pushConstantData; }
 
         void SetColor(glm::vec3 newColor);
-        void SetScale(glm::vec2 newScale);
-        void SetTransform(glm::vec2 newPos);
-        void SetRotation(float rotation);
+        void SetScale(glm::vec3 newScale);
+        void SetPosition(glm::vec3 newPos);
+        void SetRotation(glm::vec3 rotation);
+        void SetRotationX(float rotation);
+        void SetRotationY(float rotation);
+        void SetRotationZ(float rotation);
 
         private: 
         
         SnekVk::Model* model;
         SnekVk::Model::PushConstantData pushConstantData{};
-        Transform2D transform{};
+        Transform transform{};
         glm::vec3 fillColor{0.0f};
     };
 }
