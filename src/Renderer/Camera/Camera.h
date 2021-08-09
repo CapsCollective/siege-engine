a6d878ca308f8870337a1b50e36f78ef019224c7
Aryeh <aryehzinn@hotmail.com>
2021-08-10 09:34:12 +1000

    Added orthographic and perspective camera support

---
+++ src/Renderer/Camera/Camera.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../Core.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+namespace SnekVk
+{
+    class Camera
+    {
+        public:
+
+        void SetOrthographicProjection(
+            float left, float right, float top, float bottom, float near, float far);
+
+        void SetPerspectiveProjection(float fovy, float aspect, float near, float far);
+
+        const glm::mat4& GetProjection() { return projectionMatrix; }
+
+        private:
+        
+        glm::mat4 projectionMatrix{1.f};
+    };
+}
