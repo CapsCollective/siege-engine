b585ee7a209817dd77d682938ada809bb04f44d7
Aryeh <aryehzinn@hotmail.com>
2021-08-09 13:36:10 +1000

    Added 3D support, along with 3D rotations with euler angles

--- src/Components/Shape.cpp
+++ src/Components/Shape.cpp
@@ -13,21 +13,39 @@ namespace Components
         fillColor = newColor;
         pushConstantData.color = newColor;
     }
-    void Shape::SetTransform(glm::vec2 newPos) 
+    void Shape::SetPosition(glm::vec3 newPos) 
     { 
         transform.position = newPos;
-        pushConstantData.offset = newPos;
+        pushConstantData.transform = transform.transform();
     }
 
-    void Shape::SetScale(glm::vec2 newScale) 
+    void Shape::SetScale(glm::vec3 newScale) 
     { 
         transform.scale = newScale;
         pushConstantData.transform = transform.transform();
     }
 
-    void Shape::SetRotation(float rotation)
+    void Shape::SetRotation(glm::vec3 rotation)
     {
         transform.rotation = rotation;
+        pushConstantData.transform = transform.transform();
+    }
+
+    void Shape::SetRotationX(float rotation)
+    {
+        transform.rotation.x = rotation;
+        pushConstantData.transform = transform.transform();
+    }
+
+    void Shape::SetRotationY(float rotation)
+    {
+        transform.rotation.y = rotation;
+        pushConstantData.transform = transform.transform();
+    }
+
+    void Shape::SetRotationZ(float rotation)
+    {
+        transform.rotation.z = rotation;
         pushConstantData.transform = transform.transform();
     }
 }
