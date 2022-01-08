de9120064f520622c93d0469d815a68d158e74dc
Jonathan Moallem <jdmoallem@icloud.com>
2022-01-08 18:32:10 +1100

    Added standard operators and find & replace functionality to strings

--- src/utils/String.h
+++ src/utils/String.h
@@ -19,15 +19,27 @@ public:
 
     // Operator overloads
 
+    bool operator ==(const String& rhs) const;
+
     bool operator ==(const char* rhs) const;
+
+    bool operator !=(const String& rhs) const;
 
     bool operator !=(const char* rhs) const;
 
+    String operator +(const String& rhs) const;
+
     String operator +(const char* rhs) const;
+
+    String& operator =(const String& rhs);
 
     String& operator =(const char* rhs);
 
+    String& operator +=(const String& rhs);
+
     String& operator +=(const char* rhs);
+
+    char& operator [](int index);
 
     // Conversion overloads
 
@@ -39,7 +51,34 @@ public:
 
     // Public methods
 
+    // TODO add string methods for:
+    // - capacity and reservations (optimise mallocs)
+    // - searching (front to back, match, etc.)
+    // - erasing (front to back, match, etc.)
+    // - getline
+    // - iterators if necessary
+
+    // TODO copy over methods from StringHelpers
+    // - vector conversion
+    // - split
+    // - lowercase/uppercase
+    // - format
+
     bool IsEmpty() const;
+
+    size_t Size() const;
+
+    size_t Find(const String& string) const;
+
+    size_t Find(const char* string) const;
+
+    bool Replace(const char* toReplace, const char* replacement);
+
+    void Append(const String& string);
+
+    void Append(const char* string);
+
+    char At(size_t index) const;
 
     void Clear();
 
@@ -57,8 +96,6 @@ private:
 bool operator ==(const char* lhs, const String& rhs);
 
 String operator +(const char* lhs, const String& rhs);
-
-String operator +=(const char* lhs, const String& rhs);
 
 
 #endif //A_DARK_DISCOMFORT_STRING_H
--- tests/test_UtilsString.cpp
+++ tests/test_UtilsString.cpp
@@ -10,15 +10,28 @@ TEST_CASE("strings can perform basic storage operations", "[String]")
         SECTION("the string should be initialised correctly")
         {
             REQUIRE(s.IsEmpty());
+            REQUIRE(s.Size() == 0);
             REQUIRE(s == "");
             REQUIRE("" == s);
             REQUIRE(std::strcmp(s.AsChar(), "") == 0);
         }
 
-        SECTION("assigning a value via assignment operator should apply")
+        SECTION("assigning via assignment operator should apply")
+        {
+            String h("hello");
+            s = h;
+            REQUIRE(!s.IsEmpty());
+            REQUIRE(s.Size() == 5);
+            REQUIRE(s == h);
+            REQUIRE(h == s);
+            REQUIRE(std::strcmp(s.AsChar(), h.AsChar()) == 0);
+        }
+
+        SECTION("assigning a c-string via assignment operator should apply")
         {
             s = "hello";
             REQUIRE(!s.IsEmpty());
+            REQUIRE(s.Size() == 5);
             REQUIRE(s == "hello");
             REQUIRE("hello" == s);
             REQUIRE(std::strcmp(s.AsChar(), "hello") == 0);
@@ -28,6 +41,7 @@ TEST_CASE("strings can perform basic storage operations", "[String]")
         {
             s.Clear();
             REQUIRE(!s);
+            REQUIRE(s.Size() == 0);
             REQUIRE(s == "");
             REQUIRE("" == s);
             REQUIRE(std::strcmp(s.AsChar(), "") == 0);
@@ -94,14 +108,63 @@ TEST_CASE("strings can perform basic manipulation operations", "[String]")
             REQUIRE(s2 == "hellogoodbye");
             s2 = "goodbye" + s2;
             REQUIRE(s2 == "goodbyehellogoodbye");
+            REQUIRE(s2.Size() == 19);
+        }
+
+        SECTION("the string should correctly add its own type")
+        {
+            String h("hello");
+            String g("goodbye");
+            String s2 = s1 + h;
+            REQUIRE(s2 == "hello");
+            s2 = s2 + g;
+            REQUIRE(s2 == "hellogoodbye");
+            s2 = g + s2;
+            REQUIRE(s2 == "goodbyehellogoodbye");
+            REQUIRE(s2.Size() == 19);
         }
 
         SECTION("the string should correctly append c-strings")
         {
             s1 += "hello";
             REQUIRE(s1 == "hello");
-            String s2 = "goodbye" += s1;
+            String s2 = "goodbye" + s1;
             REQUIRE(s2 == "goodbyehello");
+            s2.Append("goodbye");
+            REQUIRE(s2 == "goodbyehellogoodbye");
+            REQUIRE(s2.Size() == 19);
+        }
+
+        SECTION("the string should correctly append its own type")
+        {
+            String h("hello");
+            String g("goodbye");
+            s1 += h;
+            REQUIRE(s1 == h);
+            String s2 = g;
+            s2.Append(s1);
+            REQUIRE(s2 == "goodbyehello");
+            s2.Append("goodbye");
+            REQUIRE(s2 == "goodbyehellogoodbye");
+            REQUIRE(s1 == "hello");
+            REQUIRE(s1 != "goodbyehello");
+            REQUIRE(s2.Size() == 19);
+        }
+
+        SECTION("the string should index its characters correctly")
+        {
+            s1 = "abcdefg";
+            REQUIRE(s1.At(-1) == '\0');
+            REQUIRE(s1.At(0) == 'a');
+            REQUIRE(s1.At(2) == 'c');
+            REQUIRE(s1.At(6) == 'g');
+            REQUIRE(s1.At(7) == '\0');
+            REQUIRE(s1[0] == 'a');
+            REQUIRE(s1[3] == 'd');
+            REQUIRE(s1[6] == 'g');
+            s1[1] = 'z';
+            REQUIRE(s1.At(1) == 'z');
+
         }
 
         SECTION("the string should cast correctly to c-strings")
@@ -117,5 +180,38 @@ TEST_CASE("strings can perform basic manipulation operations", "[String]")
             std::string str = s1;
             REQUIRE(str == "hellogoodbye");
         }
+    }
+}
+
+TEST_CASE("strings can perform search and replace operations", "[String]")
+{
+    String s("ruSt Is a MuCH SafEr lANgUagE...");
+    REQUIRE(s.Size() == 32);
+    String s1("lANgUagE");
+    String s2("...");
+    String s3("rUBy");
+
+    SECTION("the string can be searched for substrings")
+    {
+        REQUIRE(s.Find("ruSt") == 0);
+        REQUIRE(s.Find("a MuCH SafEr") == 8);
+        REQUIRE(s.Find(s1) == 21);
+        REQUIRE(s.Find(s2) == 29);
+        REQUIRE(s.Find(s3) == -1);
+    }
+
+    SECTION("the string can replace by substring")
+    {
+        REQUIRE(s.Replace("ruSt", s3));
+        REQUIRE(s == "rUBy Is a MuCH SafEr lANgUagE...");
+        REQUIRE(s.Find(s3) == 0);
+        REQUIRE(!s.Replace("ruSt", "NiCEr"));
+        REQUIRE(s.Replace("SafEr", "NiCEr"));
+        REQUIRE(s == "rUBy Is a MuCH NiCEr lANgUagE...");
+        REQUIRE(s.Find("NiCEr") == 15);
+        REQUIRE(s.Replace(" a MuCH NiCEr lANgUagE", ""));
+        REQUIRE(s == "rUBy Is...");
+        REQUIRE(s.Replace("...", " pyThoN wItH exTra sTepS"));
+        REQUIRE(s == "rUBy Is pyThoN wItH exTra sTepS");
     }
 }
