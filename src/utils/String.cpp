cce792e88b7beabaca3f40f6078945612c2fa612
Jonathan Moallem <jdmoallem@icloud.com>
2022-01-08 18:32:10 +1100

    Added standard operators and find & replace functionality to strings

--- src/utils/String.cpp
+++ src/utils/String.cpp
@@ -13,19 +13,30 @@ String::String(const String &rhs) :
 {}
 
 String::String(const char* rhs) :
-    str(static_cast<char*>(malloc(strlen(rhs))))
+    str(static_cast<char*>(malloc(strlen(rhs) + 1)))
 {
     strcpy(str, rhs);
 }
 
 String::~String()
 {
+    // TODO can this be removed with assertions?
     if (IsEmpty()) free(str);
+}
+
+bool String::operator ==(const String& rhs) const
+{
+    return *this == rhs.str;
 }
 
 bool String::operator ==(const char* rhs) const
 {
     return strcmp(str, rhs) == 0;
+}
+
+bool String::operator!=(const String& rhs) const
+{
+    return *this != rhs.str;
 }
 
 bool String::operator!=(const char* rhs) const
@@ -38,29 +49,36 @@ const char* String::AsChar() const
     return str;
 }
 
+String &String::operator =(const String& rhs)
+{
+    // Perform self-assignment check on internal pointer
+    return *this = rhs.str;
+}
+
 String& String::operator =(const char* rhs)
 {
+    if (str == rhs) return *this;
     free(str);
-    str = static_cast<char*>(malloc(strlen(rhs)));
+    str = static_cast<char*>(malloc(strlen(rhs) + 1));
     strcpy(str, rhs);
     return *this;
 }
 
-String &String::operator +=(const char* rhs)
+String& String::operator +=(const String& rhs)
 {
-    size_t lhsLength = strlen(str);
-    size_t rhsLength = strlen(rhs);
-    size_t fullLength = lhsLength + rhsLength;
-
-    char cstr[fullLength];
-    strcpy(cstr, str);
-    strcpy(cstr + lhsLength, rhs);
-
-    free(str);
-    str = static_cast<char*>(malloc(fullLength));
-    strcpy(str, cstr);
-
+    Append(rhs);
     return *this;
+}
+
+String& String::operator +=(const char* rhs)
+{
+    Append(rhs);
+    return *this;
+}
+
+String String::operator +(const String& rhs) const
+{
+    return *this + (const char*) rhs.str;
 }
 
 String String::operator +(const char* rhs) const
@@ -98,6 +116,77 @@ String::operator std::string() const
 bool String::IsEmpty() const
 {
     return str[0] == '\0';
+}
+
+size_t String::Size() const
+{
+    return strlen(str);
+}
+
+void String::Append(const String& string)
+{
+    Append(string.str);
+}
+
+void String::Append(const char* string)
+{
+    size_t lhsLength = strlen(str);
+    size_t rhsLength = strlen(string);
+    size_t fullLength = lhsLength + rhsLength;
+
+    char cstr[fullLength];
+    strcpy(cstr, str);
+    strcpy(cstr + lhsLength, string);
+
+    free(str);
+    str = static_cast<char*>(malloc(fullLength + 1));
+    strcpy(str, cstr);
+}
+
+char String::At(size_t index) const
+{
+    if (index < 0 || index >= Size()) return '\0';
+    return str[index];
+}
+
+char& String::operator[](int index)
+{
+    return str[index];
+}
+
+size_t String::Find(const String& string) const
+{
+    return Find(string.str);
+}
+
+size_t String::Find(const char* string) const
+{
+    char* pos = strstr(str, string);
+    return pos != nullptr ? pos - str : -1;
+}
+
+bool String::Replace(const char* toReplace, const char* replacement)
+{
+    if (!toReplace || *toReplace == '\0') return false;
+
+    // Try to find the position of the supplied substring
+    size_t pos = Find(toReplace);
+    if (pos == -1) return false;
+
+    // Replace the substring at position
+    size_t currentLen = Size();
+    size_t toReplaceLen = strlen(toReplace);
+    size_t replaceLen = strlen(replacement);
+    size_t finalLen = (currentLen - toReplaceLen) + replaceLen;
+
+    char newStr[finalLen];
+    strncpy(newStr, str, pos);
+    strncpy(newStr + pos, replacement, replaceLen);
+    strcpy(newStr + pos + replaceLen, str + pos + toReplaceLen);
+
+    // Assign the new string
+    *this = newStr;
+    return true;
 }
 
 bool operator ==(const char* lhs, const String& rhs)
