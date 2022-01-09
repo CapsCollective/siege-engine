09285adb220d7fd9256ec118afa4de5411a488d0
Jonathan Moallem <jdmoallem@icloud.com>
2022-01-09 22:11:46 +1100

    Added several string manipulation methods for prepend, case, split, replace, split, substring etc.

--- src/utils/String.h
+++ src/utils/String.h
@@ -1,7 +1,8 @@
 #ifndef A_DARK_DISCOMFORT_STRING_H
 #define A_DARK_DISCOMFORT_STRING_H
 
-#include "utility"
+#include <utility>
+#include <string>
 
 class String
 {
@@ -11,9 +12,9 @@ public:
 
     String();
 
-    String(const String& rhs);
+    String(const String& string);
 
-    String(const char* rhs);
+    String(const char* string);
 
     ~String();
 
@@ -51,40 +52,61 @@ public:
 
     // Public methods
 
-    // TODO add string methods for:
+    // TODO add functionality for:
+    // - copy and swap
     // - capacity and reservations (optimise mallocs)
-    // - searching (front to back, match, etc.)
-    // - erasing (front to back, match, etc.)
-    // - getline
-    // - iterators if necessary
 
     // TODO copy over methods from StringHelpers
     // - vector conversion
-    // - split
-    // - lowercase/uppercase
-    // - format
 
     bool IsEmpty() const;
 
     size_t Size() const;
 
+    char At(size_t index) const;
+
     size_t Find(const String& string) const;
 
     size_t Find(const char* string) const;
 
-    bool Replace(const char* toReplace, const char* replacement);
+    size_t Find(const char& character) const;
+
+    std::vector<String> Split(const char* delimiters) const;
+
+    String SubString(size_t startPos = 0, size_t length = -1) const;
 
     void Append(const String& string);
 
     void Append(const char* string);
 
-    char At(size_t index) const;
+    void Prepend(const String& string);
+
+    void Prepend(const char* string);
+
+    bool Replace(const char* toReplace, const char* replacement);
+
+    void ToUpper();
+
+    void ToLower();
 
     void Clear();
 
     const char* AsChar() const;
 
+    template <typename ...P>
+    void Format(P&&... params)
+    {
+        size_t formatLen = std::snprintf(nullptr, 0, str, std::forward<P>(params)...);
+        char newStr[formatLen];
+        std::sprintf(newStr, str, std::forward<P>(params)...);
+        Assign(newStr);
+    }
+
 private:
+
+    // Private methods
+
+    void Assign(const char* string);
 
     // Private fields
 
--- tests/test_UtilsString.cpp
+++ tests/test_UtilsString.cpp
@@ -1,4 +1,5 @@
 #include "catch.hpp"
+#include "utils/Logging.h"
 #include <utils/String.h>
 
 TEST_CASE("strings can perform basic storage operations", "[String]")
@@ -151,6 +152,33 @@ TEST_CASE("strings can perform basic manipulation operations", "[String]")
             REQUIRE(s2.Size() == 19);
         }
 
+        SECTION("the string should correctly prepend c-strings")
+        {
+            s1.Prepend("hello");
+            REQUIRE(s1 == "hello");
+            s1.Prepend("goodbye");
+            REQUIRE(s1 == "goodbyehello");
+            s1.Prepend("goodbye");
+            REQUIRE(s1 == "goodbyegoodbyehello");
+            REQUIRE(s1.Size() == 19);
+        }
+
+        SECTION("the string should correctly prepend its own type")
+        {
+            String h("hello");
+            String g("goodbye");
+            s1.Prepend(h);
+            REQUIRE(s1 == h);
+            String s2 = g;
+            s2.Prepend(s1);
+            REQUIRE(s2 == "hellogoodbye");
+            s2.Prepend("goodbye");
+            REQUIRE(s2 == "goodbyehellogoodbye");
+            REQUIRE(s1 == "hello");
+            REQUIRE(s1 != "goodbyehello");
+            REQUIRE(s2.Size() == 19);
+        }
+
         SECTION("the string should index its characters correctly")
         {
             s1 = "abcdefg";
@@ -213,5 +241,100 @@ TEST_CASE("strings can perform search and replace operations", "[String]")
         REQUIRE(s == "rUBy Is...");
         REQUIRE(s.Replace("...", " pyThoN wItH exTra sTepS"));
         REQUIRE(s == "rUBy Is pyThoN wItH exTra sTepS");
+        s.Clear();
+        REQUIRE(!s.Replace("ruSt", s3));
+        REQUIRE(s == "");
+    }
+
+    SECTION("the string can be split into substrings by delimiter")
+    {
+        std::vector<String> vec1 = s.Split(" ");
+        REQUIRE(vec1.size() == 6);
+        REQUIRE(vec1[0] == "ruSt");
+        REQUIRE(vec1[1] == "Is");
+        REQUIRE(vec1[2] == "a");
+        REQUIRE(vec1[3] == "MuCH");
+        REQUIRE(vec1[4] == "SafEr");
+        REQUIRE(vec1[5] == "lANgUagE...");
+
+        String s4("hi:my,name,is");
+        std::vector<String> vec2 = s4.Split(",");
+        REQUIRE(vec2.size() == 3);
+        REQUIRE(vec2[0] == "hi:my");
+        REQUIRE(vec2[1] == "name");
+        REQUIRE(vec2[2] == "is");
+
+        std::vector<String> vec3 = s4.Split(":,");
+        REQUIRE(vec3.size() == 4);
+        REQUIRE(vec3[0] == "hi");
+        REQUIRE(vec3[1] == "my");
+        REQUIRE(vec3[2] == "name");
+        REQUIRE(vec3[3] == "is");
+
+        String str("hello+goodbye+blah");
+        std::vector<String> vec4 = String("+" + str).Split("+");
+        REQUIRE(vec4.size() == 3);
+
+        std::vector<String> vec5 = String(str + "+").Split("+");
+        REQUIRE(vec5.size() == 3);
+
+        std::vector<String> vec6 = String("").Split("+");
+        REQUIRE(vec6.empty());
+
+        std::vector<String> vec7 = String("blah&bleh").Split("\0");
+        REQUIRE(vec7.size() == 1);
+        REQUIRE(vec7[0] == "blah&bleh");
+    }
+
+    SECTION("the string can be upper or lower-cased")
+    {
+        s.ToUpper();
+        REQUIRE(s == "RUST IS A MUCH SAFER LANGUAGE...");
+        s.ToLower();
+        REQUIRE(s == "rust is a much safer language...");
+
+        s.Clear();
+        s.ToLower();
+        REQUIRE(s.IsEmpty());
+        s = "34143523322421@#@#!@##%";
+        REQUIRE(s == "34143523322421@#@#!@##%");
+    }
+
+    SECTION("the string can be formatted")
+    {
+        String fs("This is a %s: ");
+        String fss(fs + "%s");
+        fss.Format("string", "hi");
+        REQUIRE(fss == "This is a string: hi");
+        String fsd(fs + "%d");
+        fsd.Format("decimal", 5);
+        REQUIRE(fsd == "This is a decimal: 5");
+        fsd.Format(200);
+        REQUIRE(fsd == "This is a decimal: 5");
+        fsd.Clear();
+        fsd.Format("200");
+        REQUIRE(fsd.IsEmpty());
+    }
+
+    SECTION("the string can generate substrings")
+    {
+        String sub = s.SubString();
+        REQUIRE(sub == "ruSt Is a MuCH SafEr lANgUagE...");
+        sub = s.SubString(5);
+        REQUIRE(sub == "Is a MuCH SafEr lANgUagE...");
+        sub = s.SubString(21, 8);
+        REQUIRE(sub == "lANgUagE");
+        sub = s.SubString(-1);
+        REQUIRE(!sub);
+        sub = s.SubString(0, -2);
+        REQUIRE(!sub);
+        sub = s.SubString(0, -1);
+        REQUIRE(sub == "ruSt Is a MuCH SafEr lANgUagE...");
+        sub = s.SubString(32, 4);
+        REQUIRE(!sub);
+        sub = s.SubString(31);
+        REQUIRE(sub == ".");
+        sub = s.SubString(0, 200);
+        REQUIRE(!sub);
     }
 }
