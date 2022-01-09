5de754b5e72e2b1abbc83d420df81999ec619215
Jonathan Moallem <jdmoallem@icloud.com>
2022-01-09 22:11:46 +1100

    Added several string manipulation methods for prepend, case, split, replace, split, substring etc.

--- src/utils/String.cpp
+++ src/utils/String.cpp
@@ -1,27 +1,47 @@
 #include <cstdlib>
 #include <cstring>
-#include <string>
+#include <vector>
 
 #include "String.h"
+
+static char* Allocate(const char* string)
+{
+    // Check for null pointers
+    if (!string) string = "";
+
+    // Allocate string for length and copy it over
+    char* newStr = static_cast<char*>(malloc(strlen(string) + 1));
+    strcpy(newStr, string);
+    return newStr;
+}
 
 String::String() :
     String("")
 {}
 
-String::String(const String &rhs) :
-    String(rhs.AsChar())
+String::String(const String& string) :
+    String(string.AsChar())
 {}
 
-String::String(const char* rhs) :
-    str(static_cast<char*>(malloc(strlen(rhs) + 1)))
-{
-    strcpy(str, rhs);
-}
+String::String(const char* string) :
+    str(Allocate(string))
+{}
 
 String::~String()
 {
     // TODO can this be removed with assertions?
     if (IsEmpty()) free(str);
+}
+
+void String::Assign(const char* string)
+{
+    // Check for self-assignment
+    if (str == string) return;
+
+    // TODO optimise string malloc and free
+    // Free current internal string and assign the new one
+    free(str);
+    str = Allocate(string);
 }
 
 bool String::operator ==(const String& rhs) const
@@ -49,18 +69,15 @@ const char* String::AsChar() const
     return str;
 }
 
-String &String::operator =(const String& rhs)
+String& String::operator =(const String& rhs)
 {
-    // Perform self-assignment check on internal pointer
-    return *this = rhs.str;
+    if (this != &rhs) Assign(rhs);
+    return *this;
 }
 
 String& String::operator =(const char* rhs)
 {
-    if (str == rhs) return *this;
-    free(str);
-    str = static_cast<char*>(malloc(strlen(rhs) + 1));
-    strcpy(str, rhs);
+    Assign(rhs);
     return *this;
 }
 
@@ -95,7 +112,7 @@ String String::operator +(const char* rhs) const
 
 void String::Clear()
 {
-    *this = "";
+    Assign("");
 }
 
 String::operator bool() const
@@ -138,9 +155,25 @@ void String::Append(const char* string)
     strcpy(cstr, str);
     strcpy(cstr + lhsLength, string);
 
-    free(str);
-    str = static_cast<char*>(malloc(fullLength + 1));
-    strcpy(str, cstr);
+    Assign(cstr);
+}
+
+void String::Prepend(const String& string)
+{
+    Prepend(string.str);
+}
+
+void String::Prepend(const char* string)
+{
+    size_t lhsLength = strlen(string);
+    size_t rhsLength = strlen(str);
+    size_t fullLength = lhsLength + rhsLength;
+
+    char cstr[fullLength];
+    strcpy(cstr, string);
+    strcpy(cstr + lhsLength, str);
+
+    Assign(cstr);
 }
 
 char String::At(size_t index) const
@@ -165,6 +198,12 @@ size_t String::Find(const char* string) const
     return pos != nullptr ? pos - str : -1;
 }
 
+size_t String::Find(const char& character) const
+{
+    char* pos = strchr(str, character);
+    return pos != nullptr ? pos - str : -1;
+}
+
 bool String::Replace(const char* toReplace, const char* replacement)
 {
     if (!toReplace || *toReplace == '\0') return false;
@@ -185,10 +224,50 @@ bool String::Replace(const char* toReplace, const char* replacement)
     strcpy(newStr + pos + replaceLen, str + pos + toReplaceLen);
 
     // Assign the new string
-    *this = newStr;
+    Assign(newStr);
     return true;
 }
 
+std::vector<String> String::Split(const char* delimiters) const
+{
+    if (IsEmpty()) return {};
+
+    // Copy over the internal string
+    char string[strlen(str)];
+    strcpy(string, str);
+
+    // Iterate over the string while there is still a delimiter
+    std::vector<String> strings;
+    char* substr = strtok(string, delimiters);
+    do {
+        strings.emplace_back(substr);
+    } while ((substr = strtok(nullptr, delimiters)) != nullptr);
+    return strings;
+}
+
+void String::ToUpper()
+{
+    for (int i = 0; str[i]; ++i) str[i] = (char) toupper(str[i]);
+}
+
+void String::ToLower()
+{
+    for (int i = 0; str[i]; ++i) str[i] = (char) tolower(str[i]);
+}
+
+String String::SubString(size_t startPos, size_t length) const
+{
+    // Perform boundary checking and set defaults
+    size_t strLen = Size();
+    if (length == -1) length = strLen - startPos;
+    if (startPos >= strLen || (startPos + length) > strLen) return nullptr;
+
+    // Copy over the substring and return it
+    char subString[length];
+    strncpy(subString, str + startPos, length);
+    subString[length] = '\0'; // Manually add the null-termination char
+    return subString;
+}
 bool operator ==(const char* lhs, const String& rhs)
 {
     return rhs == lhs;
