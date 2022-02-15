b2493ecff8ffbd57f3225015b75473fe9a11b65e
Jonathan Moallem <jdmoallem@icloud.com>
2022-02-16 00:02:08 +1100

    Rewrote utils tests to use utest

--- tests/utils/test_Logging.cpp
+++ tests/utils/test_Logging.cpp
@@ -1,65 +1,69 @@
-#include "../catch.hpp"
+#include <utest.h>
 
 // Require logging
 #define CC_ENABLE_LOGGING
 
 #include <utils/Logging.h>
 
-TEST_CASE("messages can be formatted correctly", "[Logging]")
+UTEST(Logging, CorrectReplacementPoints)
 {
-    SECTION("when given the correct number of points and values it should format correctly")
-    {
-        Logging::VariantContainer vc[] {1, "$$", 12.300000f, 1.2f, Vec3::Zero};
-        String fmt = "{} is {} very {} well {} formatted {}!";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "1 is $$ very 12.300000 well 1.200000 formatted Vector3(0.00,0.00,0.00)!");
-    }
+    // When given the correct number of points and values it should format correctly
+    Logging::VariantContainer vc[] {1, "$$", 12.300000f, 1.2f, Vec3::Zero};
+    String fmt = "{} is {} very {} well {} formatted {}!";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "1 is $$ very 12.300000 well 1.200000 formatted Vector3(0.00,0.00,0.00)!");
+}
 
-    SECTION("when given too few replacement points for values it should only use those it can fit")
-    {
-        Logging::VariantContainer vc[] {"hello", 17.3, "$$"};
-        String fmt = "This {} is {} too short!";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "This hello is 17.30000000000000071 too short!");
-    }
+UTEST(Logging, TooFewReplacementPoints)
+{
+    // When given too few replacement points for values it should only use those it can fit
+    Logging::VariantContainer vc[] {"hello", 17.3, "$$"};
+    String fmt = "This {} is {} too short!";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "This hello is 17.30000000000000071 too short!");
+}
 
-    SECTION("when close together replacement points and values it should format correctly")
-    {
-        Logging::VariantContainer vc[] {1, "__!", 17.3, 1.2f};
-        String fmt = "{}This {}{} too short!{}";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "1This __!17.30000000000000071 too short!1.200000");
-    }
+UTEST(Logging, CloseTogetherReplacementPoints)
+{
+    // When close together replacement points and values it should format correctly
+    Logging::VariantContainer vc[] {1, "__!", 17.3, 1.2f};
+    String fmt = "{}This {}{} too short!{}";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "1This __!17.30000000000000071 too short!1.200000");
+}
 
-    SECTION("when values contain replacement points it should still format correctly")
-    {
-        Logging::VariantContainer vc[] {"{}"};
-        String fmt = "Simple {} format!";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "Simple {} format!");
-    }
+UTEST(Logging, ReplacementPointsInValues)
+{
+    // When values contain replacement points it should still format correctly
+    Logging::VariantContainer vc[] {"{}"};
+    String fmt = "Simple {} format!";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "Simple {} format!");
+}
 
-    SECTION("when given too many points for values it should leave the rest of the message alone")
-    {
-        Logging::VariantContainer vc[] {"foo"};
-        String fmt = "This {} is {} too many!";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "This foo is {} too many!");
-    }
+UTEST(Logging, TooManyReplacementPoints)
+{
+    // When given too many points for values it should leave the rest of the message alone
+    Logging::VariantContainer vc[] {"foo"};
+    String fmt = "This {} is {} too many!";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "This foo is {} too many!");
+}
 
-    SECTION("when given an empty string to format it should do nothing")
-    {
-        Logging::VariantContainer vc[] {"foo", 23};
-        String fmt;
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt.IsEmpty());
-    }
+UTEST(Logging, EmptyFormatString)
+{
+    // When given an empty string to format it should do nothing
+    Logging::VariantContainer vc[] {"foo", 23};
+    String fmt;
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_TRUE(fmt.IsEmpty());
+}
 
-    SECTION("when given an empty replacement value string to format it should format correctly")
-    {
-        Logging::VariantContainer vc[] {"", "..."};
-        String fmt = "Almost t{}oo quiet{}";
-        Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
-        REQUIRE(fmt == "Almost too quiet...");
-    }
+UTEST(Logging, EmptyReplacementValue)
+{
+    // When given an empty replacement value string to format it should format correctly
+    Logging::VariantContainer vc[] {"", "..."};
+    String fmt = "Almost t{}oo quiet{}";
+    Logging::VariantFormat(fmt, vc, sizeof(vc) / sizeof(Logging::VariantContainer));
+    ASSERT_STREQ(fmt.Str(), "Almost too quiet...");
 }
