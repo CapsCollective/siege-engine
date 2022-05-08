dd585e6db1964c189ea83ec25b424a514e0540f4
Aryeh <aryehzinn@hotmail.com>
2022-05-08 14:15:39 +1000

    Added docs to the swapchain class

--- src/Renderer/Swapchain/Swapchain.cpp
+++ src/Renderer/Swapchain/Swapchain.cpp
@@ -63,22 +63,31 @@ namespace SnekVk
 
     void SwapChain::CreateSwapChain()
     {
+        // Get our swapchain details
         SwapChainSupportDetails::SwapChainSupportDetails details = device.GetSwapChainSupport();
 
+        // Get our supported color format
         VkSurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(details.formats, details.availableFormatCount);
+
+        // Choose our presentation mode (the form of image buffering)
         VkPresentModeKHR presentMode = ChoosePresentMode(details.presentModes, details.availablePresentModeCount);
+
+        // The size of our images. 
         VkExtent2D extent = ChooseSwapExtent(details.capabilities);
 
         std::cout << "Extent: " << extent.width << "x" << extent.height << std::endl;
 
+        // Get the image count we can support 
         u32 imageCount = details.capabilities.minImageCount + 1;
 
+        // Make sure we aren't exceeding the GPU's image count maximums
         if (details.capabilities.maxImageCount > 0 && imageCount > details.capabilities.maxImageCount)
         {
             imageCount = details.capabilities.maxImageCount;
         }   
         std::cout << "Image Count: " << imageCount << std::endl;
 
+        // Now we populate the base swapchain creation struct
         VkSwapchainCreateInfoKHR createInfo {};
         createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
         createInfo.surface = device.Surface();
@@ -90,37 +99,47 @@ namespace SnekVk
         createInfo.imageArrayLayers = 1;
         createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
         
+        // Get our image queue information for rendering
         QueueFamilyIndices::QueueFamilyIndices indices = device.FindPhysicalQueueFamilies();
         u32 queueFamilyIndices[] = {indices.graphicsFamily, indices.presentFamily};
 
+        // Sometimes the graphics and presentation queues are the same. We want to check for this 
+        // eventuality. 
         if (indices.graphicsFamily != indices.presentFamily)
         {
+            // We specify that there are two distinct queues that need to be used
             createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
             createInfo.queueFamilyIndexCount = 2;
             createInfo.pQueueFamilyIndices = queueFamilyIndices;
         }
         else 
         {
+            // Indicates that there is only a single queue to be used
             createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
             createInfo.queueFamilyIndexCount = 0;
             createInfo.pQueueFamilyIndices = nullptr;
         }
 
+        // Indicates any pre-transforms that need to occur to images (default is none)
         createInfo.preTransform = details.capabilities.currentTransform;
         createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
 
         createInfo.presentMode = presentMode;
         createInfo.clipped = VK_TRUE;
 
+        // specifies if there's an old swapchain for it to copy data from
         createInfo.oldSwapchain = VK_NULL_HANDLE;
 
         SNEK_ASSERT(vkCreateSwapchainKHR(device.Device(), &createInfo, nullptr, OUT &swapChain) == VK_SUCCESS,
                 "Failed to create Swapchain!");
 
+        // Once the swapchain has been created, get the number of images supported. 
         vkGetSwapchainImagesKHR(device.Device(), swapChain, OUT &imageCount, nullptr);
         swapChainImages = new VkImage[imageCount];
+        // Initialise the images
         vkGetSwapchainImagesKHR(device.Device(), swapChain, &imageCount, OUT swapChainImages);
 
+        // Finally, set the corresponding values in the Swapchain.
         this->imageCount = imageCount;
         swapChainImageFormat = surfaceFormat.format;
         swapChainExtent = extent;
@@ -132,6 +151,7 @@ namespace SnekVk
     {
         swapChainImageViews = new VkImageView[imageCount];
 
+        // Create our image views for each image supported:
         for (size_t i = 0; i < imageCount; i++)
         {
             VkImageViewCreateInfo createInfo{};
@@ -152,6 +172,7 @@ namespace SnekVk
 
     void SwapChain::CreateRenderPass()
     {
+        // Determine how depth will be handled in our image
         VkAttachmentDescription depthAttachment{};
         depthAttachment.format = FindDepthFormat();
         depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
@@ -166,6 +187,7 @@ namespace SnekVk
         depthAttachmentRef.attachment = 1;
         depthAttachmentRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 
+        // Specify our color attachments and how we want them to be displayed
         VkAttachmentDescription colorAttachment = {};
         colorAttachment.format = GetSwapChainImageFormat();
         colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
@@ -180,13 +202,14 @@ namespace SnekVk
         colorAttachmentRef.attachment = 0;
         colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
-
+        // Specify a rendering command that'll be followed by the render pass
         VkSubpassDescription subpass{};
         subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
         subpass.colorAttachmentCount = 1;
         subpass.pColorAttachments = &colorAttachmentRef;
         subpass.pDepthStencilAttachment = &depthAttachmentRef;
 
+        // Enforces an order in which subpasses are evaluated
         VkSubpassDependency dependency{};
         dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
         dependency.srcAccessMask = 0;
@@ -198,8 +221,11 @@ namespace SnekVk
         dependency.dstAccessMask = 
             VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
 
+        // Specify the number of attachments being used by this renderpass
         u32 attachmentCount = 2;
         VkAttachmentDescription attachments[] = {colorAttachment, depthAttachment};
+
+        // Specify our render pass creation information
         VkRenderPassCreateInfo renderPassCreateInfo{};
         renderPassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
         renderPassCreateInfo.attachmentCount = attachmentCount;
@@ -211,19 +237,24 @@ namespace SnekVk
 
         SNEK_ASSERT(vkCreateRenderPass(device.Device(), &renderPassCreateInfo, nullptr, OUT &renderPass) == VK_SUCCESS,
             "Failed to create render pass!");
+
+        // TODO: Abstract this render pass into a new object for ease of initialisation.
     }
 
     void SwapChain::CreateDepthResources()
     {
+        // Start by getting our depth information
         VkFormat format = FindDepthFormat();
         VkExtent2D swapChainExtent = GetSwapChainExtent();
 
+        // Initialise our depth image information. 
         depthImages = new VkImage[imageCount];
         depthImageMemorys = new VkDeviceMemory[imageCount];
         depthImageViews = new VkImageView[imageCount];
 
         for (size_t i = 0; i < imageCount; i++)
         {
+            // Specify the type of image
             VkImageCreateInfo imageInfo{};
             imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
             imageInfo.imageType = VK_IMAGE_TYPE_2D;
@@ -240,12 +271,14 @@ namespace SnekVk
             imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
             imageInfo.flags = 0;
 
+            // Create the depth images using the device
             device.CreateImageWithInfo(
                 imageInfo, 
                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, 
                 OUT depthImages[i], 
                 OUT depthImageMemorys[i]);
 
+            // Set up the view image creation struct
             VkImageViewCreateInfo viewInfo{};
             viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
             viewInfo.image = depthImages[i];
@@ -264,16 +297,21 @@ namespace SnekVk
 
     void SwapChain::CreateFrameBuffers()
     {
+        // Initialise the framebuffers storage
         swapChainFrameBuffers = new VkFramebuffer[imageCount];
 
+        // We need a separate frame buffer for each image that we want to draw
         for(size_t i = 0; i < imageCount; i++)
         {
+            // We have two sets of image views we need to render images with
             u32 attachmentCount = 2;
 
             VkImageView attachments[] {swapChainImageViews[i], depthImageViews[i]};
 
+            // Get our extents
             VkExtent2D swapChainExtent = GetSwapChainExtent();
 
+            // Populate the creation struct
             VkFramebufferCreateInfo frameBufferInfo{};
             frameBufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
             frameBufferInfo.renderPass = renderPass;
@@ -290,13 +328,19 @@ namespace SnekVk
 
     void SwapChain::CreateSyncObjects()
     {
+        // A semaphor for holding images that are available for use. We create one per frame in flight.
         imageAvailableSemaphores = new VkSemaphore[MAX_FRAMES_IN_FLIGHT];
+        // A semaphor for holding images that are finished rendering. We create one per frame in flight.
         renderFinishedSemaphores = new VkSemaphore[MAX_FRAMES_IN_FLIGHT];
+        
+        // A set of fences for holding our max number of images that can be swapped at once
         inFlightFences = new VkFence[MAX_FRAMES_IN_FLIGHT];
         imagesInFlight = new VkFence[imageCount];
 
+        // Set all images in flight to null
         for (size_t i = 0; i < imageCount; i++) imagesInFlight[i] = VK_NULL_HANDLE;
 
+        // Create our semaphore and fence create info
         VkSemaphoreCreateInfo semaphoreInfo{};
         semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
@@ -304,6 +348,7 @@ namespace SnekVk
         fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
         fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
+        // Create the synchronisation objects
         for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
         {
             SNEK_ASSERT(
@@ -316,6 +361,7 @@ namespace SnekVk
 
     VkResult SwapChain::AcquireNextImage(u32* imageIndex)
     {
+        // Wait for the image of the current frame to become available
         vkWaitForFences(
             device.Device(), 
             1, 
@@ -323,6 +369,7 @@ namespace SnekVk
             VK_TRUE, 
             std::numeric_limits<u64>::max());
 
+        // Once available, Add it to our available images semaphor for usage
         return vkAcquireNextImageKHR(
             device.Device(),
             swapChain, 
@@ -337,18 +384,26 @@ namespace SnekVk
     {
         u32 index = *imageIndex;
 
-        if (imagesInFlight[*imageIndex] != VK_NULL_HANDLE)
+        // If the image being asked for is being used, we wait for it to become available
+        if (imagesInFlight[index] != VK_NULL_HANDLE)
         {
             vkWaitForFences(device.Device(), 1, &imagesInFlight[index], VK_TRUE, UINT64_MAX);
         }
 
+        // Get the frame's image and move it to our images in flight
         imagesInFlight[index] = inFlightFences[currentFrame];
+
+        // With our submission, we specify a semaphore to wait on to grab data from and one to
+        // signal when the rendering is complete.
 
         VkSubmitInfo submitInfo{};
         
         submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 
+        // Specify the semaphores we want to wait for (the one for our current frame)
         VkSemaphore waitSemaphores[] = {imageAvailableSemaphores[currentFrame]};
+
+        // Set a stage that you need to wait for. In this case we wait until the color stage of the pipeline is done (fragment stage)
         VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
 
         submitInfo.waitSemaphoreCount = 1;
@@ -358,32 +413,41 @@ namespace SnekVk
         submitInfo.commandBufferCount = 1;
         submitInfo.pCommandBuffers = buffers;
 
+        // Specify some semaphores to be signalled when rendering is done
         VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
         submitInfo.signalSemaphoreCount = 1;
         submitInfo.pSignalSemaphores = signalSemaphores;
         
+        // Reset the fence of this frame
         vkResetFences(device.Device(), 1, OUT &inFlightFences[currentFrame]);
 
+        // Submit the command buffer to the graphics queue
         SNEK_ASSERT(vkQueueSubmit(device.GraphicsQueue(), 1, &submitInfo, OUT inFlightFences[currentFrame]) == VK_SUCCESS,
             "Failed to submit draw command buffer");
 
+        // Set up our presentation information and the semaphores to wait on
         VkPresentInfoKHR presentInfo{};
         presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
 
         presentInfo.waitSemaphoreCount = 1;
         presentInfo.pWaitSemaphores = signalSemaphores;
 
+        // Specify the swapchains we want to use (currently only using one)
         VkSwapchainKHR swapChains[] {swapChain};
 
         presentInfo.swapchainCount = 1;
         presentInfo.pSwapchains = swapChains;
 
+        // Store the image we want to render
         presentInfo.pImageIndices = imageIndex;
 
+        // Submit the presentation info to the present queue.
         auto result = vkQueuePresentKHR(device.PresentQueue(), &presentInfo);
 
+        // Set the frame to the next frame
         currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
 
+        // Return the result of the rendering process
         return result;
     }
 
@@ -391,13 +455,14 @@ namespace SnekVk
 
     VkSurfaceFormatKHR SwapChain::ChooseSwapSurfaceFormat(VkSurfaceFormatKHR* formats, size_t formatCount)
     {
+        // Ideally, we want to support colors in 4 dimensional vectors (R, G, B, A) in SRGB color space.
         for (size_t i = 0; i < formatCount; i++)
         {
             VkSurfaceFormatKHR& availableFormat = formats[i];
-            if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && 
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM &&
                 availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) return availableFormat;
         }
-        
+        // If we can't find what we want then we use whatever is available on the GPU.
         return formats[0];
     }
 
@@ -406,6 +471,9 @@ namespace SnekVk
         for (size_t i = 0; i < presentModeCount; i++)
         {
             VkPresentModeKHR& availablePresentMode = presentModes[i];
+
+            // Ideally, we want to support triple buffering since it has the best 
+            // balance between performance and image quality
             if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
             {
                 std::cout << "Present Mode: Mailbox" << std::endl;
@@ -413,15 +481,19 @@ namespace SnekVk
             }
         }
 
+        // If triple buffering is not available then use v-sync
         std::cout << "Present Mode: V-Sync" << std::endl;
         return VK_PRESENT_MODE_FIFO_KHR;
     }
 
     VkExtent2D SwapChain::ChooseSwapExtent(VkSurfaceCapabilitiesKHR& capabilities)
     {
+        // We want to make sure that the extents provided are within a reasonable range.
         if (capabilities.currentExtent.width != std::numeric_limits<u32>::max()) return capabilities.currentExtent;
         else 
         {
+            // Otherwise get the smaller extent between the window and the swapchain image
+            // extents and return that.
             VkExtent2D actualExtent = windowExtent;
             actualExtent.width = std::max(
                 capabilities.minImageExtent.width,
@@ -437,11 +509,15 @@ namespace SnekVk
 
     VkFormat SwapChain::FindDepthFormat()
     {
+        // The formats we want to search for:
+        // 1. A 32 bit floating point
+        // 2. A 32 bit floating point for storing depth information and 8 bits for stencil data.
+        // 3. A 32 bit component with 8 unsigned bits for stencil data and 24 unsigned normalised bits for depth. 
         VkFormat formats[] = {VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT};
         return device.FindSupportedFormat(
             formats, 
             3,
-            VK_IMAGE_TILING_OPTIMAL, 
-            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
+            VK_IMAGE_TILING_OPTIMAL, // specify we want texels to be laid out optimally
+            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT); // specifies that we can use image views to specify depth information
     }
 }
--- src/Renderer/Swapchain/Swapchain.h
+++ src/Renderer/Swapchain/Swapchain.h
@@ -4,58 +4,212 @@
 
 namespace SnekVk
 {
+    /**
+     * @brief The Swapchain class defines the creation and usage of a graphics swapchain. 
+     * Swapchains define how images are rendered to screen. Generally, rendered objects are
+     * drawn to images, which are contained within command buffers. Each frame, an image is drawn
+     * to a command buffer which is then presented for rendering.
+     * 
+     * When an image takes longer than the given time for a frame, it runs the risk of overlapping
+     * with the image being written in the following frame. This often results in screen tearing, 
+     * where two images are overlain on top of each other. 
+     * 
+     * The swapchain is therefore responsible for buffering these images by writing to a new commandbuffer 
+     * each frame. The new commandbuffer is then swapped out each frame depending on the method used. This
+     * results in smooth image transitions and safer memory usage, as frames no longer compete to render the same data. 
+     * 
+     * Swapchains are essential for computer graphics processing. 
+     */
     class SwapChain
     {
         public:
 
+        /** 
+         *  When swapping out images for our frames, we can have multiple frames 'in flight', 
+         *  meaning frames that act as additional memory buffers. In our case we allow for two additional
+         *  frames in flight for added image buffering.
+         **/
         static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
 
         // 'Structors 
 
+        /**
+         * @brief Creates a new Swapchain object to buffer our images. 
+         * 
+         * @param device a reference to a device. Devices are always destroyed last, so we can 
+         * guarantee that object destruction will occur at the correct time. 
+         * @param windowExtent the window resolution. Images need to be scaled to an extent, this usually
+         * must follow the extents of the window. 
+         */
         SwapChain(VulkanDevice& device, VkExtent2D windowExtent);
         ~SwapChain();
 
+        // Delete copy constructors. 
         SwapChain(const SwapChain&) = delete;
         void operator=(const SwapChain*) = delete;
 
         // Getters
 
+        /**
+         * @brief Images can be formatted in a number of ways. The renderer currently 
+         * searches for a specific format to render to. This function returns the format
+         * currently being used by the renderer.
+         * 
+         * @return VkFormat the format being used by the renderer. 
+         */
         VkFormat GetSwapChainImageFormat() { return swapChainImageFormat; }
-        VkExtent2D GetSwapChainExtent() { return swapChainExtent; }
 
+        /**
+         * @brief Returns the image width and height that images are being rendered to. 
+         * 
+         * @return VkExtent2D a 2D representation of the height and width of the screen. 
+         */
+        VkExtent2D GetSwapChainExtent() { return swapChainExtent; }
+        
+        /**
+         * @brief Get a frame buffer object corresponding to index i. The swapchain object stores multiple
+         * frame buffers equal to the number of images that can be held at once. 
+         * 
+         * @param i the index of the required framebuffer. NOTE: this can return a segfault if an incorrect index is provided. Please ensure that the index provided
+         * is between 0 and the swapchain's max image count. 
+         * @return VkFramebuffer the framebuffer in index i
+         */
         VkFramebuffer GetFrameBuffer(u32 i) { return swapChainFrameBuffers[i]; }
 
+        /**
+         * @brief Get the number of images that can be active at once. 
+         * 
+         * @return u32 representing the number of possible active simultaneous images. 
+         */
         u32 GetImageCount() { return imageCount; }
+
+        /**
+         * @brief The the raw Vulkan swapchain struct. 
+         * 
+         * @return VkSwapchainKHR 
+         */
         VkSwapchainKHR GetSwapChain() { return swapChain; }
 
+        /**
+         * @brief Get the swapchain image width.
+         * 
+         * @return u32 representing the width of the image. 
+         */
         u32 GetWidth() { return swapChainExtent.width; }
+
+        /**
+         * @brief Get the swapchain image height.
+         * 
+         * @return u32 representing the image height.
+         */
         u32 GetHeight() { return swapChainExtent.height; }
 
+        /**
+         * @brief Get the current render pass object. 
+         * 
+         * @return VkRenderPass - the raw vulkan render pass object. 
+         */
         VkRenderPass GetRenderPass() { return renderPass; }
 
+        /**
+         * @brief Loads in the next image to be written to in the Swapchain. 
+         * 
+         * @param imageIndex - the index of the next image. 
+         * @return VkResult - the result of acquiring the next image. 
+         */
         VkResult AcquireNextImage(u32* imageIndex);
+
+        /**
+         * @brief Submits a command buffer for drawing.
+         * 
+         * @param buffers - an array of command buffers. 
+         * @param imageIndex - the index of the image being drawn. 
+         * @return VkResult - the result of submitting the buffer. 
+         */
         VkResult SubmitCommandBuffers(const VkCommandBuffer* buffers, u32* imageIndex);
 
         private:
         
+        /**
+         * @brief Create a Vulkan swapchain object. 
+         */
         void CreateSwapChain();
+
+        /**
+         * @brief Create image views for our images. An image view represents the subresources
+         * that images must have to be valid. An image view must be created for each image being drawn.
+         */
         void CreateImageViews();
+
+        /**
+         * @brief Creates a render pass. In Vulkan, render passes are objects that represent 
+         * the state that rendering resources must be in to render to an image. All image rendering
+         * must occur within a render pass, while compute operations do not. 
+         */
         void CreateRenderPass();
+
+        /**
+         * @brief Create a Depth Resources object. Depth resources represent how object depth is calculated.
+         * These resources are written to their own resource views. 
+         */
         void CreateDepthResources();
+
+        /**
+         * @brief Creates the swapchain's frame buffers. A frame buffer represents all the data needed to be submitted 
+         * to the GPU in a frame. 
+         */
         void CreateFrameBuffers();
+
+        /**
+         * @brief Initialises all thread synchronisation objects. Vulkan is multi-threaded and submits images
+         * asynchronously. This can be dangerous when submitting images to the GPU since Vulkan does not guarantee
+         * safe execution. Therefore we must handle our own thread synchronisation. 
+         */
         void CreateSyncObjects();
 
+        /**
+         * @brief Specifies which color format we want images to be written to. Accepts a set of formats
+         * and populates them with requisite data. 
+         * 
+         * @param formats - an array of possible formats. 
+         * @param formatCount - the number of formats provided. 
+         * @return VkSurfaceFormatKHR - the chosen 
+         */
         VkSurfaceFormatKHR ChooseSwapSurfaceFormat(VkSurfaceFormatKHR* formats, size_t formatCount);
+
+        /**
+         * @brief Chooses the presentation mode that the window surface will use for images. 
+         * 
+         * @param presentModes - an array of possible presentation modes. 
+         * @param presentModeCount  - the number of presentation modes in the array. 
+         * @return VkPresentModeKHR - the chosen presentation mode. 
+         */
         VkPresentModeKHR ChoosePresentMode(VkPresentModeKHR* presentModes, size_t presentModeCount);
+
+        /**
+         * @brief Chooses an image size extent that's supported by the swapchain.
+         * 
+         * @param capabilities a reference to a struct containing our swapchain capabilities. 
+         * @return VkExtent2D - an object containing the swapchain's supported extents. 
+         */
         VkExtent2D ChooseSwapExtent(VkSurfaceCapabilitiesKHR& capabilities);
+
+        /**
+         * @brief Finds the depth format supported by this swapchain. 
+         * 
+         * @return VkFormat - the supported image format. 
+         */
         VkFormat FindDepthFormat();
         
+        // Device and window data
         VulkanDevice& device;
         VkExtent2D windowExtent;
 
+        // frame buffers and renderpasses
         VkFramebuffer* swapChainFrameBuffers;
         VkRenderPass renderPass;
 
+        // Swapchain image data
         VkFormat swapChainImageFormat;
         VkExtent2D swapChainExtent;
 
@@ -66,12 +220,15 @@ namespace SnekVk
         VkDeviceMemory* depthImageMemorys;
         VkImageView* depthImageViews;
 
+        // Images and image views supported by the swapchain
         VkImage* swapChainImages;
         VkImageView* swapChainImageViews;
         u32 imageCount;
 
+        // The raw Vulkan swapchain object
         VkSwapchainKHR swapChain;
 
+        // Synchronisation objects
         VkSemaphore* imageAvailableSemaphores;
         VkSemaphore* renderFinishedSemaphores;
         VkFence* inFlightFences;
