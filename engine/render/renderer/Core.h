a28c4b05def3a07f38d4170cefcbd2c4d5452efe
Jonathan Moallem <jdmoallem@icloud.com>
2022-05-31 09:46:58 +1000

    Refactored render lib for asset packaging and recompilation

---
+++ engine/render/renderer/Core.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "utils/Array.h"
+#include "utils/StackArray.h"
+#include "utils/Hash.h"
+
+#include <volk/volk.h>
+#include <iostream>
+#include <cstdint>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+// Custom type declarations
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int32_t i32;
+typedef size_t size;
+
+#define EXIT_APP abort();
+
+#define REPORT_ASSERT_FAILURE(expr, file, line, message) \
+    std::cout << "SNEK ASSERTION FAILURE: " << #expr << " in file: " << file << " on line: "  << line << std::endl; \
+    std::cout  << "                        Message: " << message << std::endl; 
+
+// Custom assert macro
+#define SNEK_ASSERT(expr, message) \
+    if (expr) { } \
+    else \
+    { \
+        REPORT_ASSERT_FAILURE(#expr, __FILE__, __LINE__, message); \
+        EXIT_APP \
+    } \
+
+#define OUT
+
+#define INTERN_STR(str) Utils::WSID(str)
+
+namespace SnekVk 
+{
+    struct CameraData
+    {
+        glm::mat4 projectionMatrix;
+        glm::mat4 viewMatrix;
+    };
+}
---
+++ engine/render/renderer/renderer/Renderer3D.cpp
@@ -0,0 +1,123 @@
+#include "Renderer3D.h"
+#include <iostream>
+
+namespace SnekVk
+{
+    // static initialisation
+    Utils::StringId Renderer3D::globalDataId;
+
+    Material Renderer3D::gridMaterial;
+
+    ModelRenderer Renderer3D::modelRenderer;
+    DebugRenderer3D Renderer3D::debugRenderer;
+    BillboardRenderer Renderer3D::billboardRenderer;
+    LightRenderer Renderer3D::lightRenderer;
+
+    Renderer3D::GlobalData Renderer3D::global3DData;
+
+    void Renderer3D::Initialise()
+    {
+        globalDataId = INTERN_STR("globalData");
+
+        modelRenderer.Initialise("globalData", sizeof(GlobalData));
+        debugRenderer.Initialise("globalData", sizeof(GlobalData));
+        billboardRenderer.Initialise("globalData", sizeof(GlobalData));
+        lightRenderer.Initialise("globalData", sizeof(GlobalData));
+        
+        auto gridShader = SnekVk::Shader::BuildShader()
+            .FromShader("assets/shaders/grid.vert.spv")
+            .WithStage(SnekVk::PipelineConfig::VERTEX)
+            .WithUniform(0, "globalData", sizeof(SnekVk::Renderer3D::GlobalData));
+        
+        auto gridFragShader = SnekVk::Shader::BuildShader()
+            .FromShader("assets/shaders/grid.frag.spv")
+            .WithStage(SnekVk::PipelineConfig::FRAGMENT);
+
+        gridMaterial.SetVertexShader(&gridShader);
+        gridMaterial.SetFragmentShader(&gridFragShader);
+        gridMaterial.BuildMaterial();
+    }
+
+    void Renderer3D::DrawBillboard(const glm::vec3& position, const glm::vec2& scale, const glm::vec4& colour)
+    {
+        billboardRenderer.DrawBillboard(position, scale, colour);
+    }
+
+    void Renderer3D::DrawModel(Model* model, const glm::vec3& position, const glm::vec3& scale, const glm::vec3& rotation)
+    {
+        modelRenderer.DrawModel(model, position, scale, rotation);
+    }
+
+    void Renderer3D::DrawModel(Model* model, const glm::vec3& position, const glm::vec3& scale)
+    {
+        DrawModel(model, position, scale, glm::vec3{0.f});
+    }
+
+    void Renderer3D::DrawModel(Model* model, const glm::vec3& position)
+    {
+        DrawModel(model, position, glm::vec3{1.f}, glm::vec3{0.f});
+    }
+
+    void Renderer3D::DrawPointLight(const glm::vec3& position, const float& radius, const glm::vec4& colour, const glm::vec4& ambientColor)
+    {   
+        global3DData.lightData = {colour, ambientColor, position};
+
+        lightRenderer.DrawPointLight(position, 0.05f, colour, ambientColor);
+    }
+
+    void Renderer3D::Render(VkCommandBuffer& commandBuffer, const CameraData& cameraData)
+    {
+        global3DData.cameraData = cameraData;
+        u64 globalDataSize = sizeof(global3DData);
+
+        modelRenderer.Render(commandBuffer, globalDataSize, &global3DData);
+        lightRenderer.Render(commandBuffer, globalDataSize, &global3DData);
+        debugRenderer.Render(commandBuffer, globalDataSize, &global3DData);
+        billboardRenderer.Render(commandBuffer, globalDataSize, &global3DData);
+        
+        #ifdef ENABLE_GRID
+        RenderGrid(commandBuffer, global3DData);
+        #endif
+    }
+
+    void Renderer3D::DrawLine(const glm::vec3& origin, const glm::vec3& destination, const glm::vec3& colour)
+    {
+        debugRenderer.DrawLine(origin, destination, colour);
+    }
+
+    void Renderer3D::RenderGrid(VkCommandBuffer& commandBuffer, const GlobalData& globalData)
+    {
+        gridMaterial.SetUniformData(globalDataId, sizeof(globalData), &globalData);
+        gridMaterial.Bind(commandBuffer);
+
+        vkCmdDraw(commandBuffer, 6, 1, 0, 0);
+    }
+
+    void Renderer3D::RecreateMaterials()
+    {
+        modelRenderer.RecreateMaterials();
+        debugRenderer.RecreateMaterials();
+        billboardRenderer.RecreateMaterials();
+        lightRenderer.RecreateMaterials();
+
+        gridMaterial.RecreatePipeline();
+    }
+
+    void Renderer3D::Flush()
+    {
+        modelRenderer.Flush();
+        debugRenderer.Flush();
+        billboardRenderer.Flush();
+        lightRenderer.Flush();
+    }
+
+    void Renderer3D::DestroyRenderer3D()
+    {
+        modelRenderer.Destroy();
+        debugRenderer.Destroy();
+        billboardRenderer.Destroy();
+        lightRenderer.Destroy();
+
+        gridMaterial.DestroyMaterial();
+    }
+}
