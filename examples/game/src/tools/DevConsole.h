0978d3ede623284610d994a968dce93b353107b3
Jonathan Moallem <jdmoallem@icloud.com>
2022-06-18 12:21:18 +1000

    Added copyright headers to source files

--- examples/game/src/tools/DevConsole.h
+++ examples/game/src/tools/DevConsole.h
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2022 Jonathan Moallem (@J-Mo63) & Aryeh Zinn (@Raelr)
+//
+// This code is released under an unmodified zlib license.
+// For conditions of distribution and use, please see:
+//     https://opensource.org/licenses/Zlib
+//
+
 #ifndef SIEGE_ENGINE_DEVCONSOLE_H
 #define SIEGE_ENGINE_DEVCONSOLE_H
 
