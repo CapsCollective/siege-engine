8fc9714145ac420fe479482c240ff524a3b91cce
Jonathan Moallem <jdmoallem@icloud.com>
2022-06-18 11:45:30 +1000

    Added copyright headers to Makefiles

--- make/Platform.mk
+++ make/Platform.mk
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 Jonathan Moallem (@J-Mo63) & Aryeh Zinn (@Raelr)
+#
+# This code is released under an unmodified zlib license.
+# For conditions of distribution and use, please see:
+#     https://opensource.org/licenses/Zlib
+
 # Check for Windows
 ifeq ($(OS), Windows_NT)
 	# Set Windows macros
