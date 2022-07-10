592e02995d5a3fd18f66ee182d5279fc30ee8e83
Aryeh <aryehzinn@hotmail.com>
2022-07-10 15:48:40 +1000

    Added a Matrix4x4 class and unit tests

--- engine/utils/Matrix.cpp
+++ engine/utils/Matrix.cpp
@@ -108,12 +108,12 @@ const float& Matrix2x2::operator[](const size_t& index) const
 
 bool Matrix2x2::operator==(const Matrix2x2& other)
 {
-    return MAT2_LOGIC_OP(==, &&, &&, MAT2_LOGIC_OPERATOR_BODY);
+    return MAT2_LOGIC_OP(==, &&, &&, MAT2_BODY_FUNC);
 }
 
 bool Matrix2x2::operator!=(const Matrix2x2& other)
 {
-    return MAT2_LOGIC_OP(!=, ||, ||, MAT2_LOGIC_OPERATOR_BODY);
+    return MAT2_LOGIC_OP(!=, ||, ||, MAT2_BODY_FUNC);
 }
 
 Matrix2x2& Matrix2x2::operator+=(const Matrix2x2& other)
@@ -153,15 +153,15 @@ Matrix2x2 Matrix2x2::operator-()
 
 void Matrix2x2::Add(const Matrix2x2& other)
 {
-    MAT2_MATH_OP(+=, MAT2_MATH_OPERATOR_BODY)
+    MAT2_MATH_OP(+=, MAT2_BODY_FUNC);
 }
 void Matrix2x2::Subtract(const Matrix2x2& other)
 {
-    MAT2_MATH_OP(-=, MAT2_MATH_OPERATOR_BODY)
+    MAT2_MATH_OP(-=, MAT2_BODY_FUNC);
 }
 void Matrix2x2::MultiplyScalar(const float& scalar)
 {
-    MAT2_MATH_OP(*=, MAT2_MATH_SCALAR_OPERATOR_BODY)
+    MAT2_MATH_OP(*=, MAT2_BODY_SCALAR_FUNC);
 }
 
 const float& Matrix2x2::Get(const size_t& rowIndex, const size_t& colIndex)
@@ -295,27 +295,27 @@ Matrix3x3 Matrix3x3::operator-()
 
 bool Matrix3x3::operator==(const Matrix3x3& other)
 {
-    return MAT3_LOGIC_OP(==, &&, &&, MAT3_LOGIC_OPERATOR_BODY_VEC3)
+    return MAT3_LOGIC_OP(==, &&, &&, MAT3_BODY_FUNC);
 }
 
 bool Matrix3x3::operator!=(const Matrix3x3& other)
 {
-    return MAT3_LOGIC_OP(!=, ||, ||, MAT3_LOGIC_OPERATOR_BODY_VEC3)
+    return MAT3_LOGIC_OP(!=, ||, ||, MAT3_BODY_FUNC);
 }
 
 void Matrix3x3::Add(const Matrix3x3& other)
 {
-    MAT3_MATH_OP(+=, MAT3_MATH_OPERATOR_BODY_VEC3)
+    MAT3_MATH_OP(+=, MAT3_BODY_FUNC);
 }
 
 void Matrix3x3::Subtract(const Matrix3x3& other)
 {
-    MAT3_MATH_OP(-=, MAT3_MATH_OPERATOR_BODY_VEC3)
+    MAT3_MATH_OP(-=, MAT3_BODY_FUNC);
 }
 
 void Matrix3x3::MultiplyScalar(const float& scalar)
 {
-    MAT3_MATH_OP(*=, MAT3_MATH_SCALAR_OPERATOR_BODY_VEC3)
+    MAT3_MATH_OP(*=, MAT3_BODY_SCALAR_FUNC);
 }
 
 void Matrix3x3::Multiply(const Matrix3x3& matrix)
@@ -399,6 +399,304 @@ Matrix3x3 Matrix3x3::Transpose() const
 const float& Matrix3x3::Get(const size_t& rowIndex, const size_t& colIndex)
 {
     return values[(3 * rowIndex) + colIndex];
+}
+
+// -------------------------------------- Matrix4x4 -----------------------------------------------
+
+const Matrix4x4 Matrix4x4::Identity = {1.f, 0.f, 0.f, 0.f,
+                                       0.f, 1.f, 0.f, 0.f,
+                                       0.f, 0.f, 1.f, 0.f,
+                                       0.f, 0.f, 0.f, 1.f,};
+const Matrix4x4 Matrix4x4::Zero {};
+
+// static functions
+
+Matrix4x4 Matrix4x4::Add(const Matrix4x4& lhs, const Matrix4x4& rhs)
+{
+    Matrix4x4 newMat = lhs;
+    newMat.Add(rhs);
+    return newMat;
+}
+
+Matrix4x4 Matrix4x4::Subtract(const Matrix4x4& lhs, const Matrix4x4& rhs)
+{
+    Matrix4x4 newMat = lhs;
+    newMat.Subtract(rhs);
+    return newMat;
+}
+
+Matrix4x4 Matrix4x4::MultiplyScalar(const Matrix4x4& lhs, const float& scalar)
+{
+    Matrix4x4 newMat = lhs;
+    newMat.MultiplyScalar(scalar);
+    return newMat;
+}
+
+Matrix4x4 Matrix4x4::Multiply(const Matrix4x4& lhs, const Matrix4x4& rhs)
+{
+    Matrix4x4 newMat = lhs;
+    newMat.Multiply(rhs);
+    return newMat;
+}
+
+Vec4 Matrix4x4::Multiply(const Matrix4x4& lhs, const Vec4& rhs)
+{
+    return lhs.Multiply(rhs);
+}
+
+Matrix4x4 Matrix4x4::Divide(const Matrix4x4& lhs, const Matrix4x4& rhs)
+{
+    return lhs * rhs.Inverse();
+}
+
+float Matrix4x4::Determinant(const Matrix4x4& matrix)
+{
+    return matrix.Determinant();
+}
+
+Matrix4x4 Matrix4x4::Inverse(const Matrix4x4& matrix)
+{
+    return matrix.Inverse();
+}
+
+// operators
+
+const float& Matrix4x4::operator[](const size_t& index) const
+{
+    assert(index < 16 &&
+           "Error: trying to index into matrix with a size that's greater than matrix!");
+
+    return values[index];
+}
+
+float& Matrix4x4::operator[](const size_t& index)
+{
+    assert(index < 16 &&
+           "Error: trying to index into matrix with a size that's greater than matrix!");
+
+    return values[index];
+}
+
+bool Matrix4x4::operator==(const Matrix4x4& other)
+{
+    return MAT4_LOGIC_OP(==, &&, &&, MAT4_BODY_FUNC);
+}
+
+bool Matrix4x4::operator!=(const Matrix4x4& other)
+{
+    return MAT4_LOGIC_OP(!=, ||, ||, MAT4_BODY_FUNC);
+}
+
+Matrix4x4& Matrix4x4::operator+=(const Matrix4x4& other)
+{
+    Add(other);
+    return *this;
+}
+
+Matrix4x4& Matrix4x4::operator-=(const Matrix4x4& other)
+{
+    Subtract(other);
+    return *this;
+}
+
+Matrix4x4& Matrix4x4::operator*=(const float& scalar)
+{
+    MultiplyScalar(scalar);
+    return *this;
+}
+
+Matrix4x4& Matrix4x4::operator*=(const Matrix4x4& other)
+{
+    Multiply(other);
+    return *this;
+}
+
+Matrix4x4& Matrix4x4::operator/=(const Matrix4x4& other)
+{
+    Divide(other);
+    return *this;
+}
+
+Matrix4x4 Matrix4x4::operator-()
+{
+    return {
+        {-values[0], -values[1], -values[2], -values[3]},
+        {-values[4], -values[5], -values[6], -values[7]},
+        {-values[8], -values[9], -values[10], -values[11]},
+        {-values[12], -values[13], -values[14], -values[15]}};
+}
+
+Matrix4x4 Matrix4x4::Transpose(const Matrix4x4& matrix)
+{
+    return matrix.Transpose();
+}
+
+// functions
+
+const float& Matrix4x4::Get(const size_t& rowIndex, const size_t& colIndex)
+{
+    return values[(4 * rowIndex) + colIndex];
+}
+
+void Matrix4x4::Add(const Matrix4x4& other)
+{
+    MAT4_MATH_OP(+=, MAT4_BODY_FUNC);
+}
+
+void Matrix4x4::Subtract(const Matrix4x4& other)
+{
+    MAT4_MATH_OP(-=, MAT4_BODY_FUNC);
+}
+
+void Matrix4x4::MultiplyScalar(const float& scalar)
+{
+    MAT4_MATH_OP(*=, MAT4_BODY_SCALAR_FUNC);
+}
+
+void Matrix4x4::Multiply(const Matrix4x4& rhs)
+{
+    // Rows of lhs matrix
+
+    float m0x0, m0y0, m0z0, m0w0, // Row 0
+        m0x1, m0y1, m0z1, m0w1,   // Row 1
+        m0x2, m0y2, m0z2, m0w2,
+        m0x3, m0y3, m0z3, m0w3;   // Row 2
+
+    GET_ROW4(m0x0, m0y0, m0z0, m0w0, 0)
+    GET_ROW4(m0x1, m0y1, m0z1, m0w1, 1)
+    GET_ROW4(m0x2, m0y2, m0z2, m0w2, 2)
+    GET_ROW4(m0x3, m0y3, m0z3, m0w3, 3)
+
+    // Rows of rhs matrix
+
+    float m1cx0, m1cy0, m1cz0, m1cw0,// Column 0
+        m1cx1, m1cy1, m1cz1, m1cw1,  // Column 1
+        m1cx2, m1cy2, m1cz2, m1cw2,  // Column 2
+        m1cx3, m1cy3, m1cz3, m1cw3;  // Column 3
+
+    GET_COLUMN4(m1cx0, m1cy0, m1cz0, m1cw0, rhs, 0)
+    GET_COLUMN4(m1cx1, m1cy1, m1cz1, m1cw1, rhs, 1)
+    GET_COLUMN4(m1cx2, m1cy2, m1cz2, m1cw2, rhs, 2)
+    GET_COLUMN4(m1cx3, m1cy3, m1cz3, m1cw3, rhs, 3)
+
+    values[0] = DOT4(m0x0, m0y0, m0z0, m0w0, m1cx0, m1cy0, m1cz0, m1cw0);
+    values[1] = DOT4(m0x0, m0y0, m0z0, m0w0, m1cx1, m1cy1, m1cz1, m1cw1);
+    values[2] = DOT4(m0x0, m0y0, m0z0, m0w0, m1cx2, m1cy2, m1cz2, m1cw2);
+    values[3] = DOT4(m0x0, m0y0, m0z0, m0w0, m1cx3, m1cy3, m1cz3, m1cw3);
+
+    values[4] = DOT4(m0x1, m0y1, m0z1, m0w1, m1cx0, m1cy0, m1cz0, m1cw0);
+    values[5] = DOT4(m0x1, m0y1, m0z1, m0w1, m1cx1, m1cy1, m1cz1, m1cw1);
+    values[6] = DOT4(m0x1, m0y1, m0z1, m0w1, m1cx2, m1cy2, m1cz2, m1cw2);
+    values[7] = DOT4(m0x1, m0y1, m0z1, m0w1, m1cx3, m1cy3, m1cz3, m1cw3);
+
+    values[8] = DOT4(m0x2, m0y2, m0z2, m0w2, m1cx0, m1cy0, m1cz0, m1cw0);
+    values[9] = DOT4(m0x2, m0y2, m0z2, m0w2, m1cx1, m1cy1, m1cz1, m1cw1);
+    values[10] = DOT4(m0x2, m0y2, m0z2, m0w2, m1cx2, m1cy2, m1cz2, m1cw2);
+    values[11] = DOT4(m0x2, m0y2, m0z2, m0w2, m1cx3, m1cy3, m1cz3, m1cw3);
+
+    values[12] = DOT4(m0x3, m0y3, m0z3, m0w3, m1cx0, m1cy0, m1cz0, m1cw0);
+    values[13] = DOT4(m0x3, m0y3, m0z3, m0w3, m1cx1, m1cy1, m1cz1, m1cw1);
+    values[14] = DOT4(m0x3, m0y3, m0z3, m0w3, m1cx2, m1cy2, m1cz2, m1cw2);
+    values[15] = DOT4(m0x3, m0y3, m0z3, m0w3, m1cx3, m1cy3, m1cz3, m1cw3);
+}
+
+Vec4 Matrix4x4::Multiply(const Vec4& vector) const
+{
+    return {(values[0] * vector.x) + (values[1] * vector.y) + (values[2] * vector.z) + (values[3] * vector.w),
+            (values[4] * vector.x) + (values[5] * vector.y) + (values[6] * vector.z) + (values[7] * vector.w),
+            (values[8] * vector.x) + (values[9] * vector.y) + (values[10] * vector.z) + (values[11] * vector.w),
+            (values[12] * vector.x) + (values[13] * vector.y) + (values[14] * vector.z) + (values[15] * vector.w)};
+}
+
+void Matrix4x4::Divide(const Matrix4x4& rhs)
+{
+    *this = Matrix4x4::Divide(*this, rhs);
+}
+
+float Matrix4x4::Determinant() const
+{
+    float SubFactor00 = values[10] * values[15] - values[11] * values[14];  // 1 2 3 4
+    float SubFactor01 = values[6] * values[15] - values[7] * values[14];    // 5 6 7 8
+    float SubFactor02 = values[6] * values[11] - values[7] * values[10];    // 9 0 1 2
+    float SubFactor03 = values[2] * values[15] - values[3] * values[14];    // 3 4 5 6
+    float SubFactor04 = values[2] * values[11] - values[3] * values[10];
+    float SubFactor05 = values[2] * values[7] - values[3] * values[6];
+
+    Vec4 coefficient = {
+        + (values[5] * SubFactor00 - values[9] * SubFactor01 + values[13] * SubFactor02),
+        - (values[1] * SubFactor00 - values[9] * SubFactor03 + values[13] * SubFactor04),
+        + (values[1] * SubFactor01 - values[5] * SubFactor03 + values[13] * SubFactor05),
+        - (values[1] * SubFactor02 - values[5] * SubFactor04 + values[9] * SubFactor05),
+    };
+
+    return values[0] * coefficient.x +
+           values[4] * coefficient.y +
+           values[8] * coefficient.z +
+           values[12] * coefficient.w;
+}
+
+Matrix4x4 Matrix4x4::Inverse() const
+{
+    Vec4 fac0 = {(values[10] * values[15]) - (values[14] * values[11]), // 0  1  2  3
+                 (values[10] * values[15]) - (values[14] * values[11]), // 4  5  6  7
+                 (values[6] * values[15]) -  (values[14] * values[7]),  // 8  9  10 11
+                 (values[6] * values[11]) - (values[10] * values[7])};  // 12 13 14 15
+
+    Vec4 fac1 = {values[9] * values[15] - values[13] * values[11],
+                 values[9] * values[15] - values[13] * values[11],
+                 values[5] * values[15] -  values[13] * values[7],
+                 values[5] * values[11] - values[9] * values[7]};
+
+    Vec4 fac2 = {values[9] * values[14] - values[13] * values[10],
+                 values[9] * values[14] - values[13] * values[10],
+                 values[5] * values[14] -  values[13] * values[6],
+                 values[5] * values[10] - values[9] * values[6]};
+
+    Vec4 fac3 = {values[8] * values[15] - values[12] * values[11],
+                 values[8] * values[15] - values[12] * values[11],
+                 values[4] * values[15] -  values[12] * values[7],
+                 values[4] * values[11] - values[8] * values[7]};
+
+    Vec4 fac4 = {values[8] * values[14] - values[12] * values[10],  // 0  1  2  3
+                 values[8] * values[14] - values[12] * values[10],  // 4  5  6  7
+                 values[4] * values[14] -  values[12] * values[6],  // 8  9  10 11
+                 values[4] * values[10] - values[8] * values[6]};   // 12 13 14 15
+
+    Vec4 fac5 = {values[8] * values[13] - values[12] * values[9],
+                 values[8] * values[13] - values[12] * values[9],
+                 values[4] * values[13] -  values[12] * values[5],
+                 values[4] * values[9] - values[8] * values[5]};
+
+    Vec4 vec0 = {values[4], values[0], values[0], values[0]};
+    Vec4 vec1 = {values[5], values[1], values[1], values[1]};
+    Vec4 vec2 = {values[6], values[2], values[2], values[2]};
+    Vec4 vec3 = {values[7], values[3], values[3], values[3]};
+
+    Vec4 inv0 = vec1 * fac0 - vec2 * fac1 + vec3 * fac2;
+    Vec4 inv1 = vec0 * fac0 - vec2 * fac3 + vec3 * fac4;
+    Vec4 inv2 = vec0 * fac1 - vec1 * fac3 + vec3 * fac5;
+    Vec4 inv3 = vec0 * fac2 - vec1 * fac4 + vec2 * fac5;
+
+    Vec4 signA = {+1, -1, +1, -1};
+    Vec4 signB = {-1, +1, -1, +1};
+
+    Matrix4x4 inverse = {inv0 * signA, inv1 * signB, inv2 * signA, inv3 * signB};
+
+    Vec4 dot0 = Vec4(inverse[0], inverse[4], inverse[8], inverse[12]) * Vec4(values[0], values[1], values[2], values[3]);
+    float dot = (dot0.x + dot0.y) + (dot0.z + dot0.w);
+
+    float OneOverDeterminant = 1.f / dot;
+
+    return inverse * OneOverDeterminant;
+}
+
+Matrix4x4 Matrix4x4::Transpose() const
+{
+    return {
+        values[0], values[4], values[8], values[12],
+        values[1], values[5], values[9], values[13],
+        values[2], values[6], values[10], values[14],
+        values[3], values[7], values[11], values[15]};
 }
 
 } // namespace Siege::Utils
--- engine/utils/Matrix.h
+++ engine/utils/Matrix.h
@@ -13,53 +13,82 @@
 #define X(idx, size) idx* size
 #define Y(idx, size) (idx * size) + 1
 #define Z(idx, size) (idx * size) + 2
+#define W(idx, size) (idx * size) + 3
 
 #define LHS(dimension, idx, size) values[dimension(idx, size)]
 #define RHS(dimension, idx, size) other[dimension(idx, size)]
 
-#define MAT3_MATH_OP(op, body) \
-    body(op, _SEMICOLON, 0) body(op, _SEMICOLON, 1) body(op, _SEMICOLON, 2)
+#define DEFINE_OP(op, sep, idx, element, rowSize) \
+    LHS(element, idx, rowSize) op RHS(element, idx, rowSize)
 
-#define MAT2_MATH_OP(op, body) body(op, _SEMICOLON, 0) body(op, _SEMICOLON, 1)
+#define DEFINE_SCALAR_OP(op, sep, idx, element, rowSize) \
+    LHS(element, idx, rowSize) op scalar
+
+// Matrix3x3 macros
+
+#define MAT3_MATH_OP(op, body) \
+    MAT2_MATH_OP(op, body) _SEMICOLON body(op, _SEMICOLON, 2)
 
 #define MAT3_LOGIC_OP(op, logic, bLogic, body) \
-    body(op, logic, 0) bLogic body(op, logic, 1) bLogic body(op, logic, 2);
+    MAT2_LOGIC_OP(op, logic, bLogic, body) bLogic body(op, logic, 2)
 
-#define MAT2_LOGIC_OP(op, logic, bLogic, body) body(op, logic, 0) bLogic body(op, logic, 1)
+#define MAT3_BODY_FUNC(op, sep, idx) \
+    DEFINE_OP(op, sep, idx, X, 3) sep DEFINE_OP(op, sep, idx, Y, 3) sep \
+    DEFINE_OP(op, sep, idx, Z, 3)
 
-#define MAT3_MATH_OPERATOR_BODY_VEC3(op, sep, idx) \
-    LHS(X, idx, 3)                                 \
-    op RHS(X, idx, 3) sep LHS(Y, idx, 3) op RHS(Y, idx, 3) sep LHS(Z, idx, 3) op RHS(Z, idx, 3) sep
-
-#define MAT2_MATH_OPERATOR_BODY(op, sep, idx) \
-    LHS(X, idx, 2) op RHS(X, idx, 2) sep LHS(Y, idx, 2) op RHS(Y, idx, 2) sep
-
-#define MAT3_MATH_SCALAR_OPERATOR_BODY_VEC3(op, sep, idx) \
-    LHS(X, idx, 3) op scalar sep LHS(Y, idx, 3) op scalar sep LHS(Z, idx, 3) op scalar sep
-
-#define MAT2_MATH_SCALAR_OPERATOR_BODY(op, sep, idx) \
-    LHS(X, idx, 2) op scalar sep LHS(Y, idx, 2) op scalar sep
-
-#define MAT2_LOGIC_OPERATOR_BODY(op, sep, idx) \
-    LHS(X, idx, 2) op RHS(X, idx, 2) sep LHS(Y, idx, 2) op RHS(Y, idx, 2)
-
-#define MAT3_LOGIC_OPERATOR_BODY_VEC3(op, sep, idx) \
-    LHS(X, idx, 3)                                  \
-    op RHS(X, idx, 3) sep LHS(Y, idx, 3) op RHS(Y, idx, 3) sep LHS(Z, idx, 3) op RHS(Z, idx, 3)
+#define MAT3_BODY_SCALAR_FUNC(op, sep, idx) \
+    DEFINE_SCALAR_OP(op, sep, idx, X, 3) sep DEFINE_SCALAR_OP(op, sep, idx, Y, 3) sep \
+    DEFINE_SCALAR_OP(op, sep, idx, Z, 3)
 
 #define GET_ROW3(varX, varY, varZ, idx) \
     varX = LHS(X, idx, 3), varY = LHS(Y, idx, 3), varZ = LHS(Z, idx, 3);
 
-#define GET_ROW2(varX, varY, idx) varX = LHS(X, idx, 2), varY = LHS(Y, idx, 2);
-
 #define GET_COLUMN3(varX, varY, varZ, vals, idx) \
     varX = vals[idx], varY = vals[idx + 3], varZ = vals[idx + 6];
+
+#define DOT3(m0x, m0y, m0z, m1x, m1y, m1z) DOT(m0x, m0y, m1x, m1y) + (m0z * m1z)
+
+// Matrix2x2 macros
+
+#define MAT2_MATH_OP(op, body) body(op, _SEMICOLON, 0) _SEMICOLON body(op, _SEMICOLON, 1)
+
+#define MAT2_LOGIC_OP(op, logic, bLogic, body) body(op, logic, 0) bLogic body(op, logic, 1)
+
+#define MAT2_BODY_FUNC(op, sep, idx)                       \
+    DEFINE_OP(op, sep, idx, X, 2) sep DEFINE_OP(op, sep, idx, Y, 2) \
+
+#define MAT2_BODY_SCALAR_FUNC(op, sep, idx) \
+    DEFINE_SCALAR_OP(op, sep, idx, X, 2) sep DEFINE_SCALAR_OP(op, sep, idx, Y, 2)
+
+#define GET_ROW2(varX, varY, idx) varX = LHS(X, idx, 2), varY = LHS(Y, idx, 2);
 
 #define GET_COLUMN2(varX, varY, vals, idx) varX = vals[idx], varY = vals[idx + 2];
 
 #define DOT(m0x, m0y, m1x, m1y) (m0x * m1x) + (m0y * m1y)
 
-#define DOT3(m0x, m0y, m0z, m1x, m1y, m1z) DOT(m0x, m0y, m1x, m1y) + (m0z * m1z)
+// Matrix4x4 macros
+
+#define MAT4_MATH_OP(op, body) MAT3_MATH_OP(op, body) _SEMICOLON body(op, _SEMICOLON, 3)
+
+#define MAT4_LOGIC_OP(op, logic, bLogic, body) \
+    MAT3_LOGIC_OP(op, logic, bLogic, body) bLogic body(op, logic, 3)
+
+#define MAT4_BODY_FUNC(op, sep, idx) \
+    DEFINE_OP(op, sep, idx, X, 4) sep DEFINE_OP(op, sep, idx, Y, 4) sep \
+    DEFINE_OP(op, sep, idx, Z, 4) sep DEFINE_OP(op, sep, idx, W, 4)
+
+#define MAT4_BODY_SCALAR_FUNC(op, sep, idx) \
+    DEFINE_SCALAR_OP(op, sep, idx, X, 4) sep DEFINE_SCALAR_OP(op, sep, idx, Y, 4) sep \
+    DEFINE_SCALAR_OP(op, sep, idx, Z, 4) sep DEFINE_SCALAR_OP(op, sep, idx, W, 4)
+
+#define GET_ROW4(varX, varY, varZ, varW, idx) \
+    varX = LHS(X, idx, 4), varY = LHS(Y, idx, 4), varZ = LHS(Z, idx, 4), varW = LHS(W, idx, 4);
+
+#define GET_COLUMN4(varX, varY, varZ, varW, vals, idx) \
+    varX = vals[idx], varY = vals[idx + 4], varZ = vals[idx + 8], varW = vals[idx + 12];
+
+#define DOT4(m0x, m0y, m0z, m0w, m1x, m1y, m1z, m1w) \
+    DOT3(m0x, m0y, m0z, m1x, m1y, m1z) + (m0w * m1w)
 
 namespace Siege::Utils
 {
@@ -256,14 +285,104 @@ inline Matrix3x3 operator/(const Matrix3x3& lhs, const Matrix3x3& rhs)
     return Matrix3x3::Divide(lhs, rhs);
 }
 
-// struct Matrix4
-//{
-// public:
-//
-// private:
-//
-//     float values[16];
-// };
+ struct Matrix4x4
+{
+ public:
+     Matrix4x4(const float& x0 = 0.f, const float& y0 = 0.f, const float& z0 = 0.f, const float& w0 = 0.f,
+               const float& x1 = 0.f, const float& y1 = 0.f, const float& z1 = 0.f, const float& w1 = 0.f,
+               const float& x2 = 0.f, const float& y2 = 0.f, const float& z2 = 0.f, const float& w2 = 0.f,
+               const float& x3 = 0.f, const float& y3 = 0.f, const float& z3 = 0.f, const float& w3 = 0.f)
+         : values {x0, y0, z0, w0, x1, y1, z1, w1, x2, y2, z2, w2, x3, y3, z3, w3} {}
+
+     Matrix4x4(const Vec4& row0, const Vec4& row1, const Vec4& row2, const Vec4& row3)
+         : Matrix4x4(row0.x, row0.y, row0.z, row0.w,
+                   row1.x, row1.y, row1.z, row1.w,
+                   row2.x, row2.y, row2.z, row2.w,
+                   row3.x, row3.y, row3.z, row3.w) {}
+
+     // public const variables
+
+     static const Matrix4x4 Identity;
+     static const Matrix4x4 Zero;
+
+     // static functions
+
+     static Matrix4x4 Add(const Matrix4x4& lhs, const Matrix4x4& rhs);
+     static Matrix4x4 Subtract(const Matrix4x4& lhs, const Matrix4x4& rhs);
+     static Matrix4x4 MultiplyScalar(const Matrix4x4& lhs, const float& scalar);
+     static Matrix4x4 Multiply(const Matrix4x4& lhs, const Matrix4x4& rhs);
+     static Vec4 Multiply(const Matrix4x4& lhs, const Vec4& rhs);
+     static Matrix4x4 Divide(const Matrix4x4& lhs, const Matrix4x4& rhs);
+
+     static float Determinant(const Matrix4x4& matrix);
+     static Matrix4x4 Inverse(const Matrix4x4& matrix);
+     static Matrix4x4 Transpose(const Matrix4x4& matrix);
+
+     // operators
+
+     const float& operator[](const size_t& index) const;
+     float& operator[](const size_t& index);
+
+     bool operator==(const Matrix4x4& other);
+     bool operator!=(const Matrix4x4& other);
+
+     Matrix4x4& operator+=(const Matrix4x4& other);
+     Matrix4x4& operator-=(const Matrix4x4& other);
+     Matrix4x4& operator*=(const float& scalar);
+     Matrix4x4& operator*=(const Matrix4x4& other);
+     Matrix4x4& operator/=(const Matrix4x4& other);
+
+     Matrix4x4 operator-();
+
+     // functions
+
+     const float& Get(const size_t& rowIndex, const size_t& colIndex);
+     void Add(const Matrix4x4& other);
+     void Subtract(const Matrix4x4& other);
+     void MultiplyScalar(const float& scalar);
+     void Multiply(const Matrix4x4& rhs);
+     Vec4 Multiply(const Vec4& vector) const;
+     void Divide(const Matrix4x4& rhs);
+
+     float Determinant() const;
+     Matrix4x4 Inverse() const;
+     Matrix4x4 Transpose() const;
+
+ private:
+
+     float values[16];
+ };
+
+ inline Matrix4x4 operator+(const Matrix4x4& lhs, const Matrix4x4& rhs)
+ {
+     return Matrix4x4::Add(lhs, rhs);
+ }
+
+ inline Matrix4x4 operator-(const Matrix4x4& lhs, const Matrix4x4& rhs)
+ {
+     return Matrix4x4::Subtract(lhs, rhs);
+ }
+
+ inline Matrix4x4 operator*(const Matrix4x4& lhs, const float& scalar)
+ {
+     return Matrix4x4::MultiplyScalar(lhs, scalar);
+ }
+
+ inline Matrix4x4 operator*(const Matrix4x4& lhs, const Matrix4x4& rhs)
+ {
+     return Matrix4x4::Multiply(lhs, rhs);
+ }
+
+ inline Vec4 operator*(const Matrix4x4& lhs, const Vec4& rhs)
+ {
+     return Matrix4x4::Multiply(lhs, rhs);
+ }
+
+ inline Matrix4x4 operator/(const Matrix4x4& lhs, const Matrix4x4& rhs)
+ {
+     return Matrix4x4::Divide(lhs, rhs);
+ }
+
 
 } // namespace Siege::Utils
 
