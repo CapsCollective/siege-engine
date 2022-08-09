92a97ad4f779f2f5ba2e22df503aef35580b7e40
Aryeh <aryeh_zinn@mckinsey.com>
2022-08-09 15:30:19 +1000

    Added the Managed StackArray Class (MSArray) and unit tests

--- engine/utils/StackArray.h
+++ engine/utils/StackArray.h
@@ -70,29 +70,20 @@ public:
          * @brief The dereference operator.
          * @return the de-referenced pointer value.
          */
-        inline T& operator*()
-        {
-            return *pointer;
-        }
+        inline T& operator*() { return *pointer; }
         /**
          * @brief An equal operator, checks with equality.
          * @param other the other iterator to compare to.
          * @return `true` if the pointers stored by both iterators are the same.
          */
-        inline bool operator==(const Iterator& other) const
-        {
-            return pointer == other.pointer;
-        }
+        inline bool operator==(const Iterator& other) const { return pointer == other.pointer; }
 
         /**
          * @brief A non equality operator. Checks that twio iterators are not equal.
          * @param other the iterator to compare to.
          * @return `true` if the pointer addresses stored by both iterators are not the same.
          */
-        inline bool operator!=(const Iterator& other) const
-        {
-            return pointer != other.pointer;
-        }
+        inline bool operator!=(const Iterator& other) const { return pointer != other.pointer; }
 
         T* pointer;
     };
@@ -203,6 +194,320 @@ public:
     constexpr inline const Iterator end() const { return Iterator(data+S); }
 
     T data[S];
+};
+
+template<typename T, size_t S>
+/**
+ * @brief The Managed Stack Array (MSArray) is a managed stack-allocated array type. The MSArray
+ * is intended to be used when a container is needed who's size is known at compile time, but is
+ * required to be constant once a size is provided.
+ *
+ * The MSArray tracks all data that's been inserted and removed from the array. All data inserted
+ * into the array will have its index marked as active. Accessing an index which has not been
+ * activated will trigger an immediate assertion failure. As such, the HeapArray wants you to
+ * only access data which has been explicitly entered into it, thus avoiding issues where 'garbage'
+ * data may be accessed through.
+ *
+ * The MSArray is a 'safer' alternative to the StackArray(SArray), however it does sacrifice memory
+ * usage for safety.
+ * @tparam T the type being stored by the array.
+ * @tparam S the static size of the array.
+ */
+class MSArray
+{
+public:
+    /**
+     * @brief A base iterator for the Managed Stack Array (MSArray) class. This iterator moves through all active
+     * elements in the array and returns them. As such, all data points not explicitly inserted
+     * into the array will be ignored.
+     */
+    class Iterator
+    {
+    public:
+        // TODO(Aryeh): Add more operators as needed (--, ->, etc).
+        /**
+         * @brief Iterator constructor.
+         * @param valuePointer the raw array pointer.
+         * @param newMaskPointer the pointer to our state array.
+         * @param newIndex the starting index.
+         * @param newSize the array size.
+         */
+        explicit Iterator(T* valuePointer,
+                          uint8_t* newMaskPointer,
+                          const size_t& newIndex)
+            : pointer {valuePointer}, maskPointer {newMaskPointer}, index {newIndex}
+        {}
+
+        /**
+         * @brief The iterator prefix increment operator. This operator will increment the pointer
+         * as many times as needed to find an active element. As such, it will not return unactive
+         * array elements.
+         * @return the iterator with the pointer and index incremented.
+         */
+        Iterator& operator++()
+        {
+            // If the next element in the array is invalid, keep incrementing until we find one that
+            // is.
+            while (IsNextIndexInvalid()) Increment();
+
+            Increment();
+
+            return *this;
+        }
+
+        /**
+         * @brief The postfix increment operator. Does the same thing as the postfix operator, but
+         * applies it after returning the object.
+         * @return the iterator before it was incremented.
+         */
+        const Iterator operator++(int)
+        {
+            Iterator iterator = *this;
+            ++(*this);
+            return iterator;
+        }
+
+        /**
+         * @brief The dereference operator.
+         * @return the de-referenced pointer value.
+         */
+        T& operator*()
+        {
+            return *pointer;
+        }
+
+        /**
+         * @brief Increments the pointer and index
+         */
+        void Increment()
+        {
+            pointer++;
+            index++;
+        }
+
+        /**
+         * @brief Checks the next index to see if it's active.
+         * @return `true` if the next index is inactive, `false` if it is.
+         */
+        bool IsNextIndexInvalid()
+        {
+            // Get the chunk of bytes we want to use to search for states.
+            uint8_t byteMask = maskPointer[(index + 1) / BYTE_SIZE_IN_BITS];
+
+            /**
+             * Get a bit representation of the position we want to search for.
+             * For example, given a bit position in index 1, we create a bit mask with a value of
+             * 1 left shifted by 1 position (0000 0010). Finally, since our bytes contain 8 bits,
+             * we want to make sure that we just check the index we get is relative to our byte.
+             * i.e: 2 % 8 = 2 or 9 % 8 = 1. No matter how large the index, we'll always get its
+             * position in 8s.
+             */
+            auto bitPosition = (1 << ((index + 1) % BYTE_SIZE_IN_BITS));
+
+            /**
+             * Run a bitwise AND operation to check if the corresponding byte exists in the original
+             * mask. i.e: 1010 0010 & 0000 0010 will return 1 (since 1 exists in the second bit).
+             *
+             * Finally, we also want to make sure the next index is in bounds.
+             */
+            return (byteMask & bitPosition) == 0 && (index + 1) < S;
+        }
+
+        /**
+         * @brief An equal operator, checks with equality.
+         * @param other the other iterator to compare to.
+         * @return `true` if the pointers stored by both iterators are the same.
+         */
+        bool operator==(const Iterator& other) const {return pointer == other.pointer;}
+
+        /**
+         * @brief A non equality operator. Checks that twio iterators are not equal.
+         * @param other the iterator to compare to.
+         * @return `true` if the pointer addresses stored by both iterators are not the same.
+         */
+        bool operator!=(const Iterator& other) const { return pointer != other.pointer; }
+
+    private:
+        T* pointer;
+        uint8_t* maskPointer;
+        size_t index;
+    };
+    MSArray() = default;
+
+    MSArray(std::initializer_list<T> list) : count{list.size()}
+    {
+        CopyData(std::data(list), list.size()); // 0000 1111
+        SetBitsToOne(list.size());
+
+        count = list.size();
+    }
+
+    MSArray(const T* ptr, const size_t& ptrSize)
+    {
+        CopyData(ptr, ptrSize);
+        SetBitsToOne(ptrSize);
+
+        count = ptrSize;
+    }
+
+    MSArray(MSArray& other)
+    {
+        Copy(other);
+    }
+
+    MSArray(MSArray&& other)
+    {
+        Copy(other);
+        other.count = 0;
+    }
+
+    // Operator Overloads
+
+    inline const T& operator[](const size_t& index) const { return Get(index); }
+
+    inline T& operator[](const size_t& index) {
+        VerifyIndex(index);
+        return Get(index);
+    }
+
+    inline MSArray& operator=(const MSArray& other)
+    {
+        Copy(other);
+        return *this;
+    }
+
+    inline MSArray& operator=(MSArray&& other)
+    {
+        Copy(other);
+        other.count = 0;
+        return *this;
+    }
+
+    // Getters
+
+    inline const T& Get(const size_t& index) const
+    {
+        AssertIsInBoundsAndActive(index);
+        return data[index];
+    }
+
+    inline T& Get(const size_t& index) { return data[index]; }
+
+    inline const T* Data() const { return data; }
+    inline T* Data() { return data; }
+    inline constexpr const size_t Size() const { return S; }
+    inline const size_t Count() const { return count; }
+
+    // Setters
+
+    inline void Insert(const size_t& index, const T& element)
+    {
+        VerifyIndex(index);
+        Set(index, element);
+    }
+
+    inline bool IsInBounds(const size_t& index) const { return index < S; }
+    inline bool Empty() const { return count == 0; }
+
+    inline bool Active(const size_t& index) const
+    {
+        auto byteMask = stateMaskBitfield[index / BYTE_SIZE_IN_BITS];
+        return (byteMask & (1 << (index % BYTE_SIZE_IN_BITS))) != 0;
+    }
+
+    inline void Remove(const size_t& index)
+    {
+        AssertIsInBoundsAndActive(index);
+        stateMaskBitfield[index / BYTE_SIZE_IN_BITS] &= ~(1 << (index % BYTE_SIZE_IN_BITS));
+        count--;
+    }
+
+    inline void Clear()
+    {
+        memset(stateMaskBitfield, 0, (BYTE_MASK_SIZE * GetBitMaskSize()));
+        count = 0;
+    }
+
+    inline const Iterator begin() const {return Iterator(data, stateMaskBitfield, 0);}
+    inline const Iterator end() const {return Iterator(data + S, stateMaskBitfield, S);}
+
+    inline Iterator begin() {return Iterator(data, stateMaskBitfield, 0);}
+    inline Iterator end() {return Iterator(data + S, stateMaskBitfield, S);}
+private:
+    // TODO: Move this to a third intermediate file
+    // The number of states we can fit per segment (1 byte = 8 states)
+    static inline constexpr uint8_t BYTE_SIZE_IN_BITS = 8;
+    // The default object size for each segment (1 byte)
+    static inline constexpr uint8_t BYTE_MASK_SIZE = sizeof(uint8_t);
+    // Each position represents a byte where all bits before the index are 1.
+    // i.e: index 2 = 0000 00111
+    static inline constexpr uint8_t MAX_BIT_VALUES[BYTE_SIZE_IN_BITS] = {1, 3, 7, 15, 31, 63, 127, 255};
+
+    constexpr inline size_t GetBitMaskSize() { return (S / BYTE_SIZE_IN_BITS) + 1; }
+
+    inline void Set(const size_t& index, const T& element) { data[index] = element; }
+
+    inline void VerifyIndex(const size_t& index)
+    {
+        AssertIsInBounds(index);
+        count += !Active(index);
+        stateMaskBitfield[index / BYTE_SIZE_IN_BITS] |= (1 << (index % BYTE_SIZE_IN_BITS));
+    }
+
+    inline void Copy(const MSArray& other)
+    {
+        CopyData(other.Data(), other.Size());
+        CopyMask(other.stateMaskBitfield, GetBitMaskSize());
+        count = other.count;
+    }
+
+    inline void AssertIsInBoundsAndActive(const size_t& index) const
+    {
+        AssertIsInBounds(index);
+        AssertIsActive(index);
+    }
+
+    inline void AssertIsInBounds(const size_t& index) const
+    {
+        assert(IsInBounds(index) && "The provided index was not in bounds!");
+    }
+
+    inline void AssertIsActive(const size_t& index) const
+    {
+        assert(Active(index) && "Element does not exist in the array!");
+    }
+
+    inline void CopyData(const T* src, const size_t& srcSize)
+    {
+        memcpy(data, src, sizeof(T) * srcSize);
+    }
+
+    inline void CopyMask(const uint8_t* source, const size_t maskSize)
+    {
+        memcpy(stateMaskBitfield, source, BYTE_MASK_SIZE * maskSize);
+    }
+
+    inline void SetBitsToOne(const size_t& bits)
+    {
+        uint8_t bitsToProcess = bits;
+        size_t newByteCount = (bitsToProcess / BYTE_SIZE_IN_BITS) + 1;
+
+        for (size_t i = 0; i < newByteCount; i++)
+        {
+            bool isFullByte = (bitsToProcess - BYTE_SIZE_IN_BITS) >= 0;
+
+            uint8_t maskBits = (bitsToProcess * !isFullByte) + (BYTE_SIZE_IN_BITS * isFullByte);
+
+            stateMaskBitfield[i] =
+                MAX_BIT_VALUES[std::clamp<size_t>(maskBits - 1, 0, BYTE_SIZE_IN_BITS)];
+
+            bitsToProcess -= BYTE_SIZE_IN_BITS;
+        }
+    }
+
+    T data[S];
+    uint8_t stateMaskBitfield[(S / BYTE_SIZE_IN_BITS) + 1] {0};
+    size_t count {0};
 };
 }
 
--- tests/utils/test_StackArray.cpp
+++ tests/utils/test_StackArray.cpp
@@ -11,9 +11,11 @@
 
 #include <utility>
 
+//---------------------------------------- SArray --------------------------------------------------
+
 UTEST(test_SArray, CreateEmptySMSArray)
 {
-    Siege::Utils::SArray<uint32_t, 1> array;
+    Siege::Utils::SArray<uint32_t, 1> array = {};
 
     ASSERT_TRUE(array.Size() == 1);
     ASSERT_TRUE(array.Data() != nullptr);
@@ -155,7 +157,7 @@ UTEST(test_SArray, CopySArray)
 
 UTEST(test_SArray, CopyEmptySArray)
 {
-    Siege::Utils::SArray<uint32_t, 0> arrayA;
+    Siege::Utils::SArray<uint32_t, 0> arrayA{};
     Siege::Utils::SArray<uint32_t, 0> arrayB(arrayA);
 
     arrayA = arrayB;
@@ -214,6 +216,390 @@ UTEST(test_SArray, IterateOverSingleElementSArray)
     Siege::Utils::SArray<uint32_t ,1> array = {1};
 
     uint32_t expectedResults[] = {1};
+
+    size_t index = 0;
+    for (auto& element : array)
+    {
+        ASSERT_EQ(expectedResults[index++], element);
+    }
+}
+
+//---------------------------------------- MSArray -------------------------------------------------
+
+UTEST(test_MSArray, CreateEmptyMSArray)
+{
+    Siege::Utils::MSArray<uint32_t, 0> array;
+
+    ASSERT_EQ(0, array.Size());
+    ASSERT_EQ(0, array.Count());
+    ASSERT_TRUE(array.Data() != nullptr);
+}
+
+UTEST(test_MSArray, CreateMSArrayWithSize)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    ASSERT_EQ(2, array.Size());
+    ASSERT_EQ(0, array.Count());
+    ASSERT_FALSE(array.Data() == nullptr);
+}
+
+UTEST(test_MSArray, CreateMSArrayWithInitializerList)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array = {0, 1};
+
+    ASSERT_TRUE(array.Size() == 2);
+    ASSERT_TRUE(array.Count() == 2);
+    ASSERT_FALSE(array.Data() == nullptr);
+
+    ASSERT_EQ(array[0], 0);
+    ASSERT_EQ(array[1], 1);
+
+    ASSERT_EQ(2, array.Count());
+}
+
+UTEST(test_MSArray, CreateMSArrayFromCopyConstructor)
+{
+    Siege::Utils::MSArray<uint32_t, 2> arrayA = {0, 1};
+    Siege::Utils::MSArray<uint32_t, 2> arrayB(arrayA);
+
+    ASSERT_EQ(arrayA.Size(), arrayB.Size());
+    ASSERT_EQ(arrayA.Count(), arrayB.Count());
+
+    ASSERT_EQ(arrayA[0], arrayB[0]);
+    ASSERT_EQ(arrayA[1], arrayB[1]);
+}
+
+UTEST(test_MSArray, CopyEmptyArrayWithConstructor)
+{
+    Siege::Utils::MSArray<uint32_t, 0> arrayA;
+
+    Siege::Utils::MSArray<uint32_t, 0> arrayB(arrayA);
+
+    ASSERT_NE(arrayA.Data(), arrayB.Data());
+    ASSERT_EQ(arrayA.Count(), arrayB.Count());
+    ASSERT_EQ(arrayA.Size(), arrayB.Size());
+}
+
+UTEST(test_MSArray, CreateMSArrayFromMoveConstructor)
+{
+    Siege::Utils::MSArray<uint32_t, 4> arrayA = {1, 2, 3, 4};
+
+    ASSERT_EQ(arrayA.Count(), 4);
+    ASSERT_EQ(arrayA.Size(), 4);
+    ASSERT_NE(arrayA.Data(), nullptr);
+
+    ASSERT_EQ(arrayA[0], 1);
+    ASSERT_EQ(arrayA[1], 2);
+    ASSERT_EQ(arrayA[2], 3);
+    ASSERT_EQ(arrayA[3], 4);
+
+    Siege::Utils::MSArray<uint32_t, 4> arrayB(std::move(arrayA));
+
+    ASSERT_EQ(arrayB.Count(), 4);
+    ASSERT_EQ(arrayB.Size(), 4);
+    ASSERT_NE(arrayB.Data(), nullptr);
+
+    ASSERT_EQ(arrayB[0], 1);
+    ASSERT_EQ(arrayB[1], 2);
+    ASSERT_EQ(arrayB[2], 3);
+    ASSERT_EQ(arrayB[3], 4);
+}
+
+UTEST(test_MSArray, CreateMSArrayFromRawArray)
+{
+    uint32_t values[] = {1, 2, 3, 4};
+    Siege::Utils::MSArray<uint32_t, 4> array(values, 4);
+
+    ASSERT_EQ(array.Count(), 4);
+    ASSERT_EQ(array.Size(), 4);
+    ASSERT_NE(array.Data(), nullptr);
+
+    ASSERT_TRUE(array.Active(0));
+    ASSERT_TRUE(array.Active(1));
+    ASSERT_TRUE(array.Active(2));
+    ASSERT_TRUE(array.Active(3));
+
+    ASSERT_EQ(array[0], 1);
+    ASSERT_EQ(array[1], 2);
+    ASSERT_EQ(array[2], 3);
+    ASSERT_EQ(array[3], 4);
+}
+
+UTEST(test_MSArray, ClearArray)
+{
+    Siege::Utils::MSArray<uint32_t, 4> array = {1, 2, 3, 4};
+    ASSERT_EQ(array.Count(), 4);
+    ASSERT_EQ(array.Size(), 4);
+    ASSERT_NE(array.Data(), nullptr);
+
+    ASSERT_EQ(array[0], 1);
+    ASSERT_EQ(array[1], 2);
+    ASSERT_EQ(array[2], 3);
+    ASSERT_EQ(array[3], 4);
+
+    array.Clear();
+
+    ASSERT_EQ(array.Count(), 0);
+}
+
+UTEST(test_MSArray, TestIsEmpty)
+{
+    Siege::Utils::MSArray<uint32_t, 4> array = {1, 2, 3, 4};
+    ASSERT_EQ(array.Count(), 4);
+    ASSERT_EQ(array.Size(), 4);
+    ASSERT_NE(array.Data(), nullptr);
+
+    ASSERT_EQ(array[0], 1);
+    ASSERT_EQ(array[1], 2);
+    ASSERT_EQ(array[2], 3);
+    ASSERT_EQ(array[3], 4);
+
+    ASSERT_FALSE(array.Empty());
+
+    array.Clear();
+
+    ASSERT_EQ(array.Count(), 0);
+    ASSERT_TRUE(array.Empty());
+}
+
+UTEST(test_MSArray, CheckIndexInBounds)
+{
+    Siege::Utils::MSArray<uint32_t, 1> array;
+
+    ASSERT_TRUE(array.IsInBounds(0));
+    ASSERT_FALSE(array.IsInBounds(1));
+}
+
+UTEST(test_MSArray, InsertElementUsingFunction)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    ASSERT_TRUE(array.Empty());
+
+    array.Insert(0, 1);
+
+    ASSERT_FALSE(array.Empty());
+    ASSERT_EQ(1, array.Count());
+
+    array.Insert(1, 2);
+
+    ASSERT_EQ(2, array.Count());
+
+    ASSERT_EQ(1, array[0]);
+    ASSERT_EQ(2, array[1]);
+}
+
+UTEST(test_MSArray, InsertElementUsingBrackets)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    ASSERT_TRUE(array.Empty());
+
+    array[0] = 1;
+
+    ASSERT_FALSE(array.Empty());
+    ASSERT_EQ(1, array.Count());
+
+    array[1] = 2;
+
+    ASSERT_EQ(2, array.Count());
+
+    ASSERT_EQ(1, array[0]);
+    ASSERT_EQ(2, array[1]);
+}
+
+UTEST(test_MSArray, GetElementUsingFunction)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    array[0] = 1;
+
+    ASSERT_EQ(array.Get(0), 1);
+
+    array[1] = 2;
+
+    ASSERT_EQ(array.Get(1), 2);
+}
+
+UTEST(test_MSArray, GetElementUsingBrackets)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    array[0] = 1;
+
+    ASSERT_EQ(array[0], 1);
+
+    array[1] = 2;
+
+    ASSERT_EQ(array[1], 2);
+}
+
+UTEST(test_MSArray, CheckElementIsActive)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    array[0] = 1;
+
+    ASSERT_TRUE(array.Active(0));
+    ASSERT_FALSE(array.Active(1));
+
+    array[1] = 2;
+
+    ASSERT_TRUE(array.Active(1));
+}
+
+UTEST(test_MSArray, RemoveElementByIndex)
+{
+    Siege::Utils::MSArray<uint32_t, 2> array;
+
+    array[0] = 1;
+
+    ASSERT_EQ(array.Count(), 1);
+    ASSERT_TRUE(array.Active(0));
+
+    array[1] = 2;
+
+    ASSERT_EQ(array.Count(), 2);
+    ASSERT_TRUE(array.Active(1));
+
+    array.Remove(1);
+
+    ASSERT_EQ(array.Count(), 1);
+    ASSERT_FALSE(array.Active(1));
+
+    array.Remove(0);
+
+    ASSERT_EQ(array.Count(), 0);
+    ASSERT_FALSE(array.Active(0));
+    ASSERT_TRUE(array.Empty());
+}
+
+UTEST(test_MSArray, CopyArray)
+{
+    Siege::Utils::MSArray<uint32_t, 2> arrayA = {0, 1};
+    Siege::Utils::MSArray<uint32_t, 2> arrayB = {3, 4};
+
+    arrayA = arrayB;
+
+    ASSERT_EQ(arrayA[0], arrayB[0]);
+    ASSERT_EQ(arrayA[1], arrayB[1]);
+
+    ASSERT_EQ(arrayA.Size(), arrayB.Size());
+    ASSERT_EQ(arrayA.Count(), arrayB.Count());
+}
+
+UTEST(test_MSArray, CopyEmptyArray)
+{
+    Siege::Utils::MSArray<uint32_t, 0> arrayA;
+    Siege::Utils::MSArray<uint32_t, 0> arrayB(arrayA);
+
+    arrayA = arrayB;
+
+    ASSERT_EQ(arrayA.Size(), arrayB.Size());
+    ASSERT_EQ(arrayA.Count(), arrayB.Count());
+}
+
+UTEST(test_MSArray, CopyEmptyArrayToFullArray)
+{
+    Siege::Utils::MSArray<uint32_t, 4> arrayA;
+
+    Siege::Utils::MSArray<uint32_t, 4> arrayB = { 1, 2, 3, 4 };
+
+    arrayB = arrayA;
+
+    ASSERT_EQ(arrayA.Count(), arrayB.Count());
+    ASSERT_EQ(arrayA.Size(), arrayB.Size());
+}
+
+UTEST(test_MSArray, MoveArray)
+{
+    Siege::Utils::MSArray<uint32_t, 2> arrayA;
+
+    ASSERT_EQ(arrayA.Size(), 2);
+    ASSERT_EQ(arrayA.Count(), 0);
+
+    Siege::Utils::MSArray<uint32_t, 2> arrayB = {3, 4};
+
+    ASSERT_EQ(arrayB.Size(), 2);
+    ASSERT_EQ(arrayB.Count(), 2);
+
+    ASSERT_NE(arrayB.Data(), nullptr);
+
+    ASSERT_EQ(arrayB[0], 3);
+    ASSERT_EQ(arrayB[1], 4);
+
+    arrayA = std::move(arrayB);
+
+    ASSERT_EQ(arrayA.Size(), 2);
+    ASSERT_EQ(arrayA.Count(), 2);
+
+    ASSERT_EQ(arrayA[0], 3);
+    ASSERT_EQ(arrayA[1], 4);
+}
+
+UTEST(test_MSArray, IterateUsingBrackets)
+{
+    Siege::Utils::MSArray<uint32_t, 4> array({1, 2, 3, 4});
+
+    uint32_t expectedResults[] = {1, 2, 3, 4};
+
+    for (size_t i = 0; i < array.Count(); i++)
+    {
+        ASSERT_EQ(array[i], expectedResults[i]);
+    }
+}
+
+UTEST(test_MSArray, IterateUsingRangedForLoop)
+{
+    Siege::Utils::MSArray<uint32_t, 4> array = {1, 2, 3, 4};
+
+    uint32_t expectedResults[] = {1, 2, 3, 4};
+
+    size_t index = 0;
+    for (auto& element : array)
+    {
+        ASSERT_EQ(expectedResults[index++], element);
+    }
+}
+
+UTEST(test_MSArray, IterateUsingRangedForLoopWithPartiallyFilledArray)
+{
+    Siege::Utils::MSArray<uint32_t, 4> array;
+
+    array[0] = 1;
+    array[1] = 2;
+    array[3] = 4;
+
+    uint32_t expectedResults[] = {1, 2, 4, 5};
+
+    size_t index = 0;
+    for (auto& element : array)
+    {
+        ASSERT_EQ(expectedResults[index++], element);
+    }
+}
+
+UTEST(test_MSArray, IterateOverSingleElementArray)
+{
+    Siege::Utils::MSArray<uint32_t, 1> array = {1};
+
+    uint32_t expectedResults[] = {1};
+
+    size_t index = 0;
+    for (auto& element : array)
+    {
+        ASSERT_EQ(expectedResults[index++], element);
+    }
+}
+
+UTEST(test_MSArray, IterateOverLargeArray)
+{
+    Siege::Utils::MSArray<uint32_t, 20> array =
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+    uint32_t expectedResults[]
+        = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
     size_t index = 0;
     for (auto& element : array)
