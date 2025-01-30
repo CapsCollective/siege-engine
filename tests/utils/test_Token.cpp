1f8fc631d52050cf75c20ed834206427904a7eaa
Jonathan Moallem <jdmoallem@icloud.com>
2025-01-30 22:31:18 +1000

    Added test for heap allocated string tokens

--- tests/utils/test_Token.cpp
+++ tests/utils/test_Token.cpp
@@ -13,6 +13,7 @@
 REGISTER_TOKEN(HelloWorld);
 REGISTER_TOKEN(NewOne);
 REGISTER_TOKEN(GoodbyeWorld);
+REGISTER_TOKEN(ReallyLongHeapAllocatedStringTokenName);
 
 UTEST(test_Token, StaticallyRegistered)
 {
@@ -22,6 +23,7 @@ UTEST(test_Token, StaticallyRegistered)
     Siege::Token tk4 = TOKEN_HelloWorld;
     Siege::Token tk5("Hi");
     Siege::Token tk6("HelloWorld");
+    Siege::Token tk7 = TOKEN_ReallyLongHeapAllocatedStringTokenName;
 
     ASSERT_TRUE(tk1.IsValid());
     ASSERT_TRUE(tk2.IsValid());
@@ -29,20 +31,26 @@ UTEST(test_Token, StaticallyRegistered)
     ASSERT_TRUE(tk4.IsValid());
     ASSERT_TRUE(!tk5.IsValid());
     ASSERT_TRUE(tk6.IsValid());
+    ASSERT_TRUE(tk7.IsValid());
 
     ASSERT_STREQ("HelloWorld", tk1.GetId());
     ASSERT_STREQ("NewOne", tk2.GetId());
     ASSERT_STREQ("GoodbyeWorld", tk3.GetId());
     ASSERT_STREQ("HelloWorld", tk4.GetId());
     ASSERT_STREQ("HelloWorld", tk6.GetId());
+    ASSERT_STREQ("ReallyLongHeapAllocatedStringTokenName", tk7.GetId());
 
     ASSERT_TRUE(tk1 != tk2);
     ASSERT_TRUE(tk1 != tk3);
     ASSERT_TRUE(tk1 == tk4);
     ASSERT_TRUE(tk1 == tk6);
+    ASSERT_TRUE(tk1 != tk7);
     ASSERT_TRUE(tk2 != tk3);
     ASSERT_TRUE(tk2 != tk4);
+    ASSERT_TRUE(tk2 != tk7);
     ASSERT_TRUE(tk3 != tk4);
+    ASSERT_TRUE(tk3 != tk7);
+    ASSERT_TRUE(tk6 != tk7);
 }
 
 UTEST(test_Token, DynamicallyRegistered)
