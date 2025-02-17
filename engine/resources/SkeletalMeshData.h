ae527f9a3035d945d2c748236a3cdcfeca521c22
Jonathan Moallem <jdmoallem@icloud.com>
2025-02-17 22:32:20 +1000

    Implemented named bone packing for skeletal mesh data

--- engine/resources/SkeletalMeshData.h
+++ engine/resources/SkeletalMeshData.h
@@ -14,26 +14,28 @@
 #include <utils/math/vec/Hashing.h>
 #include <utils/math/vec/Vec2.h>
 #include <utils/math/vec/Vec3.h>
+#include <utils/math/mat/Mat4.h>
 
 #include "StaticMeshData.h"
 #include "PackFile.h"
 
 namespace Siege
 {
-
-struct SkinnedVertex
+struct Bone
 {
-    BaseVertex vertex;
-    Vec3 position;
-    FColour color;
-    Vec3 normal;
-    Vec2 uv;
+    uint32_t id;
+    Mat4 bindMatrix;
+};
+
+struct SkinnedVertex : public BaseVertex
+{
+    Vec4 bones;
+    Vec4 weights;
 };
 
 inline bool operator==(const SkinnedVertex& left, const SkinnedVertex& right)
 {
-    return left.position == right.position && left.color == right.color &&
-           left.normal == right.normal && left.uv == right.uv;
+    return static_cast<BaseVertex>(left) == static_cast<BaseVertex>(right) && left.bones == right.bones && left.weights == right.weights;
 }
 
 inline bool operator!=(const SkinnedVertex& left, const SkinnedVertex& right)
@@ -46,6 +48,8 @@ struct SkeletalMeshData
 {
     uint64_t indicesCount = 0;
     uint64_t verticesCount = 0;
+    uint32_t bonesCount = 0;
+    uint32_t boneNamesLength = 0;
     char data[];
 
     uint32_t* GetIndices() const
@@ -58,30 +62,66 @@ struct SkeletalMeshData
         return (SkinnedVertex*) (data + sizeof(uint32_t) * indicesCount);
     }
 
-    static SkeletalMeshData* Create(const std::vector<uint32_t>& objIndices,
-                                  const std::vector<SkinnedVertex>& objVertices)
+    Bone* GetBones() const
     {
+        return (Bone*) (data + (sizeof(uint32_t) * indicesCount + sizeof(SkinnedVertex) * verticesCount));
+    }
+
+    char* GetBoneNames() const
+    {
+        return (char*) (data + (sizeof(uint32_t) * indicesCount + sizeof(SkinnedVertex) * verticesCount) + sizeof(Bone) * bonesCount);
+    }
+
+    static SkeletalMeshData* Create(const std::vector<uint32_t>& objIndices,
+                                    const std::vector<SkinnedVertex>& objVertices,
+                                    const std::map<Siege::String, Siege::Bone>& objBones)
+    {
+        std::vector<Siege::Bone> bones;
+        std::vector<Siege::String> boneNames;
+        size_t boneNamesLength = 0;
+        for (const auto& it : objBones)
+        {
+            bones.push_back(it.second);
+            boneNames.push_back(it.first);
+            boneNamesLength += it.first.Size() + 1;
+        }
+
+        char boneNamesArr[boneNamesLength];
+        size_t currentBoneNamesOffset = 0;
+        for (const Siege::String& name : boneNames)
+        {
+            memcpy(&boneNamesArr[0] + currentBoneNamesOffset, name.Str(), name.Size() + 1);
+            currentBoneNamesOffset += name.Size() + 1;
+        }
+
         uint32_t indicesDataSize = sizeof(uint32_t) * objIndices.size();
         uint32_t verticesDataSize = sizeof(SkinnedVertex) * objVertices.size();
-        uint32_t totalDataSize = sizeof(SkeletalMeshData) + indicesDataSize + verticesDataSize;
+        uint32_t bonesDataSize = sizeof(Bone) * objBones.size();
+        uint32_t totalDataSize = sizeof(SkeletalMeshData) + indicesDataSize + verticesDataSize + bonesDataSize + boneNamesLength;
 
         void* mem = malloc(totalDataSize);
-        SkeletalMeshData* staticMeshData = new (mem) SkeletalMeshData();
+        SkeletalMeshData* skeletalMeshData = new (mem) SkeletalMeshData();
 
-        staticMeshData->indicesCount = objIndices.size();
-        staticMeshData->verticesCount = objVertices.size();
-        memcpy(&staticMeshData->data[0], objIndices.data(), indicesDataSize);
-        memcpy(&staticMeshData->data[0] + indicesDataSize, objVertices.data(), verticesDataSize);
+        skeletalMeshData->indicesCount = objIndices.size();
+        skeletalMeshData->verticesCount = objVertices.size();
+        skeletalMeshData->bonesCount = objBones.size();
+        skeletalMeshData->boneNamesLength = boneNamesLength;
+        memcpy(&skeletalMeshData->data[0], objIndices.data(), indicesDataSize);
+        memcpy(&skeletalMeshData->data[0] + indicesDataSize, objVertices.data(), verticesDataSize);
 
-        return staticMeshData;
+        memcpy(&skeletalMeshData->data[0] + indicesDataSize + verticesDataSize, bones.data(), bonesDataSize);
+        memcpy(&skeletalMeshData->data[0] + indicesDataSize + verticesDataSize + bonesDataSize, boneNamesArr, boneNamesLength);
+
+        return skeletalMeshData;
     }
 
     static uint32_t GetDataSize(void* objectData)
     {
         if (!objectData) return 0;
-        SkeletalMeshData* staticMeshData = reinterpret_cast<SkeletalMeshData*>(objectData);
-        return sizeof(SkeletalMeshData) + sizeof(uint32_t) * staticMeshData->indicesCount +
-               sizeof(SkinnedVertex) * staticMeshData->verticesCount;
+        SkeletalMeshData* skeletalMeshData = reinterpret_cast<SkeletalMeshData*>(objectData);
+        return sizeof(SkeletalMeshData) + sizeof(uint32_t) * skeletalMeshData->indicesCount +
+               sizeof(SkinnedVertex) * skeletalMeshData->verticesCount +
+               sizeof(Bone) * skeletalMeshData->bonesCount + skeletalMeshData->boneNamesLength;
     }
 };
 #pragma pack(pop)
@@ -96,8 +136,8 @@ struct hash<Siege::SkinnedVertex>
 {
     size_t operator()(const Siege::SkinnedVertex& vertex) const
     {
-        size_t seed = 0;
-        Siege::Hash::HashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+        size_t seed;
+        Siege::Hash::HashCombine(seed, vertex, vertex.bones, vertex.weights);
         return seed;
     };
 };
--- packer/src/types/SkeletalMeshDataPacker.cpp
+++ packer/src/types/SkeletalMeshDataPacker.cpp
@@ -21,15 +21,29 @@
 
 #include "StaticMeshDataPacker.h"
 
+static inline Siege::Mat4 ConvertMatrix(const aiMatrix4x4& from)
+{
+    Siege::Mat4 to;
+    //the a,b,c,d in assimp is the row ; the 1,2,3,4 is the column
+    to[0][0] = from.a1; to[1][0] = from.a2; to[2][0] = from.a3; to[3][0] = from.a4;
+    to[0][1] = from.b1; to[1][1] = from.b2; to[2][1] = from.b3; to[3][1] = from.b4;
+    to[0][2] = from.c1; to[1][2] = from.c2; to[2][2] = from.c3; to[3][2] = from.c4;
+    to[0][3] = from.d1; to[1][3] = from.d2; to[2][3] = from.d3; to[3][3] = from.d4;
+    return to;
+}
+
 static void GetMeshData(const aiScene* scene,
                         aiMesh* mesh,
                         aiMatrix4x4t<ai_real> matrix,
                         OUT std::vector<Siege::SkinnedVertex>& vertices,
-                        OUT std::vector<uint32_t>& indices)
+                        OUT std::vector<uint32_t>& indices,
+                        OUT std::map<Siege::String, Siege::Bone>& bones)
 {
     for (uint32_t i = 0; i < mesh->mNumVertices; i++)
     {
         Siege::SkinnedVertex vertex {};
+        vertex.bones = {-1, -1, -1, -1};
+        vertex.weights = {0.f, 0.f, 0.f, 0.f };
 
         aiVector3t<ai_real> vert = mesh->mVertices[i];
         vert *= matrix;
@@ -50,59 +64,84 @@ static void GetMeshData(const aiScene* scene,
         vertices.push_back(vertex);
     }
 
-    auto it = std::max_element(indices.begin(), indices.end());
-    uint32_t indexOffset = it != indices.end() ? *it + 1 : 0;
     for (uint32_t i = 0; i < mesh->mNumFaces; i++)
     {
         aiFace face = mesh->mFaces[i];
         for (uint32_t j = 0; j < face.mNumIndices; j++)
         {
-            indices.push_back(indexOffset + face.mIndices[j]);
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+
+    for (uint32_t i = 0; i < mesh->mNumBones; i++)
+    {
+        aiBone* currentBone = mesh->mBones[i];
+
+        uint32_t boneId;
+        Siege::String boneName(currentBone->mName.C_Str());
+
+        auto it = bones.find(boneName);
+        if (it != bones.end())
+        {
+            boneId = it->second.id;
+        }
+        else
+        {
+            boneId = bones.size();
+
+            Siege::Bone newBone;
+            newBone.id = boneId;
+            newBone.bindMatrix = ConvertMatrix(currentBone->mOffsetMatrix);
+            bones.emplace(boneName, newBone);
+        }
+
+        CC_LOG_INFO("Reading bone {} with id {}", boneName, boneId)
+
+        for (int32_t j = 0; j < currentBone->mNumWeights; ++j)
+        {
+            aiVertexWeight weight = currentBone->mWeights[j];
+            Siege::SkinnedVertex vertex = vertices[weight.mVertexId];
+            for (int32_t k = 0; k < 4; ++k)
+            {
+                if (vertex.bones[k] < 0)
+                {
+                    vertex.bones[k] = static_cast<float>(boneId);
+                    vertex.weights[k] = weight.mWeight;
+                    break;
+                }
+            }
         }
     }
 }
 
-static void GetMeshesForNode(const aiScene* scene,
-                             aiNode* node,
-                             const Siege::String& requestPath,
-                             Siege::String currentPath,
-                             aiMatrix4x4t<ai_real> matrix,
-                             OUT std::vector<Siege::SkinnedVertex>& vertices,
-                             OUT std::vector<uint32_t>& indices)
+static aiMesh* FindMeshAtPath(const aiScene* scene, aiNode* node, const Siege::String& requestPath, Siege::String currentPath)
 {
     currentPath = currentPath + node->mName.C_Str();
-
     CC_LOG_INFO("Reading node at {}", currentPath)
-
-    matrix *= node->mTransformation;
 
     for (uint32_t i = 0; i < node->mNumMeshes; i++)
     {
         aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-
         Siege::String currentMeshPath = currentPath + '/' + mesh->mName.C_Str();
-
         CC_LOG_INFO("Reading mesh at {}", currentMeshPath)
-        GetMeshData(scene, mesh, matrix, vertices, indices);
+
+        if (currentMeshPath != requestPath) continue;
+        return mesh;
     }
 
     for (uint32_t i = 0; i < node->mNumChildren; i++)
     {
-        GetMeshesForNode(scene,
-                         node->mChildren[i],
-                         requestPath,
-                         currentPath + '/',
-                         matrix,
-                         vertices,
-                         indices);
+        aiMesh* mesh = FindMeshAtPath(scene, node->mChildren[i], requestPath, currentPath + '/');
+        if (mesh) return mesh;
     }
-};
+
+    return nullptr;
+}
 
 void* PackSkeletalMeshFile(const Siege::String& filePath, const Siege::String& assetsPath)
 {
     Siege::String contents = Siege::FileSystem::Read(filePath);
-    std::map<Siege::Token, Siege::String> attributes =
-        Siege::FileSystem::ParseAttributeFileData(contents);
+    std::map<Siege::Token, Siege::String> attributes = Siege::FileSystem::ParseAttributeFileData(contents);
 
     if (attributes.empty())
     {
@@ -129,22 +168,36 @@ void* PackSkeletalMeshFile(const Siege::String& filePath, const Siege::String& a
         return nullptr;
     }
 
-    auto nodePathIt = attributes.find(TOKEN_NODE_PATH);
-    Siege::String requestedNodePath = nodePathIt != attributes.end() ? nodePathIt->second : "/";
+    if (!scene->HasMeshes())
+    {
+        CC_LOG_WARNING("File at path \"{}\" contains no meshes", modelPath)
+        return nullptr;
+    }
 
-    CC_LOG_INFO("Reading skeletal mesh for file {} with node path {}", it->second, requestedNodePath)
+    aiMesh* mesh = nullptr;
+    auto nodePathIt = attributes.find(TOKEN_NODE_PATH);
+    if (nodePathIt != attributes.end())
+    {
+        Siege::String requestedNodePath = nodePathIt->second;
+        CC_LOG_INFO("Reading skeletal mesh for file {} with path {}", it->second, requestedNodePath)
+        mesh = FindMeshAtPath(scene, scene->mRootNode, requestedNodePath, '/');
+        if (!mesh)
+        {
+            CC_LOG_WARNING("Failed to find mesh at path \"{}\"", requestedNodePath)
+            return nullptr;
+        }
+    }
+    else
+    {
+        CC_LOG_INFO("Reading skeletal mesh for file {}", it->second)
+        mesh = scene->mMeshes[0];
+    }
 
     std::vector<Siege::SkinnedVertex> vertices;
     std::vector<uint32_t> indices;
-    GetMeshesForNode(scene,
-                     scene->mRootNode,
-                     requestedNodePath,
-                     '/',
-                     aiMatrix4x4t<ai_real>(),
-                     vertices,
-                     indices);
+    std::map<Siege::String, Siege::Bone> bones;
+    GetMeshData(scene, mesh, aiMatrix4x4t<ai_real>(), vertices, indices, bones);
 
-//    Siege::SkeletalMeshData* skeletalMeshData = Siege::SkeletalMeshData::Create(indices, vertices);
-//    return skeletalMeshData;
-    return nullptr;
+    Siege::SkeletalMeshData* skeletalMeshData = Siege::SkeletalMeshData::Create(indices, vertices, bones);
+    return skeletalMeshData;
 }
